aec24007937a526b899298cefba0ef9966cca770
Simmypeet <simmypeet@outlook.com>
2024-02-06 00:55:05 +0700

    Add mutable visit

--- compiler/pernixc_semantic/src/semantic/visitor.rs
+++ compiler/pernixc_semantic/src/semantic/visitor.rs
@@ -23,12 +23,24 @@ pub trait Visitor {
     #[must_use]
     fn visit_lifetime(&mut self, lifetime: &Lifetime) -> bool;
 
-    /// Visits a constant.  
+    /// Visits a constant.
     ///
     /// Returns `true` if the visitor should continue visiting the sub-terms of
     /// the constant.
     #[must_use]
     fn visit_constant(&mut self, constant: &Constant) -> bool;
+
+    /// Similar to [`Self::visit_type()`], but for mutable references.
+    #[must_use]
+    fn visit_type_mut(&mut self, ty: &mut Type) -> bool;
+
+    /// Similar to [`Self::visit_lifetime()`], but for mutable references.
+    #[must_use]
+    fn visit_lifetime_mut(&mut self, lifetime: &mut Lifetime) -> bool;
+
+    /// Similar to [`Self::visit_constant()`], but for mutable references.
+    #[must_use]
+    fn visit_constant_mut(&mut self, constant: &mut Constant) -> bool;
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Error)]
@@ -65,6 +77,30 @@ impl<'a, V: Visitor> Visitor for RecursiveVisitorAdapter<'a, V> {
 
         constant.accept_one_level(self).unwrap_or(true)
     }
+
+    fn visit_type_mut(&mut self, ty: &mut Type) -> bool {
+        if !self.visitor.visit_type_mut(ty) {
+            return false;
+        }
+
+        ty.accept_one_level_mut(self).unwrap_or(true)
+    }
+
+    fn visit_lifetime_mut(&mut self, lifetime: &mut Lifetime) -> bool {
+        if !self.visitor.visit_lifetime_mut(lifetime) {
+            return false;
+        }
+
+        lifetime.accept_one_level_mut(self).unwrap_or(true)
+    }
+
+    fn visit_constant_mut(&mut self, constant: &mut Constant) -> bool {
+        if !self.visitor.visit_constant_mut(constant) {
+            return false;
+        }
+
+        constant.accept_one_level_mut(self).unwrap_or(true)
+    }
 }
 
 /// A term for the visitor pattern.
@@ -75,6 +111,9 @@ pub trait Element {
     ///
     /// Returns whatever the visitor `visit_*` method returns.
     fn accept_single(&self, visitor: &mut impl Visitor) -> bool;
+
+    /// Similar to [`Self::accept_single()`], but for mutable references.
+    fn accept_single_mut(&mut self, visitor: &mut impl Visitor) -> bool;
 
     /// Visits one sub-level of the term.
     ///
@@ -95,6 +134,16 @@ pub trait Element {
     /// When visiting a non-application term (i.e. `int32, float32`)
     fn accept_one_level(
         &self,
+        visitor: &mut impl Visitor,
+    ) -> Result<bool, VisitNonApplicationTermError>;
+
+    /// Similar to [`Self::accept_one_level()`], but for mutable references.
+    ///
+    /// # Errors
+    ///
+    /// When visiting a non-application term (i.e. `int32, float32`)
+    fn accept_one_level_mut(
+        &mut self,
         visitor: &mut impl Visitor,
     ) -> Result<bool, VisitNonApplicationTermError>;
 }
@@ -126,47 +175,133 @@ pub fn accept_recursive(
     }
 }
 
+/// Similar to [`accept_recursive()`], but for mutable references.
+pub fn accept_recursive_mut(
+    element: &mut impl Element,
+    visitor: &mut impl Visitor,
+) -> bool {
+    if !element.accept_single_mut(visitor) {
+        return false;
+    }
+
+    let mut adapter = RecursiveVisitorAdapter { visitor };
+
+    match element.accept_one_level_mut(&mut adapter) {
+        Ok(result) => result,
+        Err(VisitNonApplicationTermError) => true,
+    }
+}
+
+macro_rules! implements_tuple {
+    ($self:ident, $visitor:ident, $accept_single:ident $(, $ref:ident)?) => {
+        {
+            for element in &$($ref)? $self.elements {
+                if !match element {
+                    TupleElement::Regular(term) | TupleElement::Unpacked(term) => {
+                        term.$accept_single($visitor)
+                    }
+                } {
+                    return false;
+                }
+            }
+
+            true
+        }
+    };
+}
+
 impl<Term: Element + Clone> Tuple<Term>
 where
     Self: TryFrom<Term, Error = Term> + Into<Term>,
 {
     fn accept_one_level(&self, visitor: &mut impl Visitor) -> bool {
-        for element in &self.elements {
-            if !match element {
-                TupleElement::Regular(term) | TupleElement::Unpacked(term) => {
-                    term.accept_single(visitor)
-                }
-            } {
-                return false;
-            }
-        }
-
-        true
+        implements_tuple!(self, visitor, accept_single)
     }
+
+    fn accept_one_level_mut(&mut self, visitor: &mut impl Visitor) -> bool {
+        implements_tuple!(self, visitor, accept_single_mut, mut)
+    }
+}
+
+macro_rules! implements_generic_arguments {
+    ($self:ident, $visitor:ident, $visit_type:ident, $visit_lifetime:ident, $visit_constant:ident $(, $ref:ident)?) => {
+        {
+            for lifetime in &$($ref)? $self.lifetimes {
+                if !$visitor.$visit_lifetime(lifetime) {
+                    return false;
+                }
+            }
+
+            for ty in &$($ref)? $self.types {
+                if !$visitor.$visit_type(ty) {
+                    return false;
+                }
+            }
+
+            for constant in &$($ref)? $self.constants {
+                if !$visitor.$visit_constant(constant) {
+                    return false;
+                }
+            }
+
+            true
+        }
+    };
 }
 
 impl GenericArguments {
     fn accept_one_level(&self, visitor: &mut impl Visitor) -> bool {
-        for lifetime in &self.lifetimes {
-            if !visitor.visit_lifetime(lifetime) {
-                return false;
-            }
-        }
-
-        for ty in &self.types {
-            if !visitor.visit_type(ty) {
-                return false;
-            }
-        }
-
-        for constant in &self.constants {
-            if !visitor.visit_constant(constant) {
-                return false;
-            }
-        }
-
-        true
+        implements_generic_arguments!(
+            self,
+            visitor,
+            visit_type,
+            visit_lifetime,
+            visit_constant
+        )
     }
+
+    fn accept_one_level_mut(&mut self, visitor: &mut impl Visitor) -> bool {
+        implements_generic_arguments!(
+            self,
+            visitor,
+            visit_type_mut,
+            visit_lifetime_mut,
+            visit_constant_mut,
+            mut
+        )
+    }
+}
+
+macro_rules! implements_type {
+    ($self:ident, $visitor:ident, $visit_type:ident, $visit_lifetime:ident, $visit_constant:ident, $accept_one_level:ident $(, $ref:ident)?) => {
+        match $self {
+            Self::Primitive(_) | Self::Parameter(_) | Self::Inference(_) => {
+                Err(VisitNonApplicationTermError)
+            }
+
+            Self::Symbol(term) => {
+                if !term.generic_arguments.$accept_one_level($visitor) {
+                    return Ok(false);
+                }
+
+                Ok(true)
+            }
+            Self::Pointer(term) => Ok($visitor.$visit_type(&$($ref)? term.pointee)),
+            Self::Reference(term) => Ok($visitor
+                .$visit_lifetime(&$($ref)?term.lifetime)
+                && $visitor.$visit_type(&$($ref)?term.pointee)),
+            Self::Array(term) => Ok($visitor.$visit_type(&$($ref)?term.r#type)
+                && $visitor.$visit_constant(&$($ref)? term.length)),
+            Self::Tuple(tuple) => Ok(tuple.$accept_one_level($visitor)),
+            Self::Local(local) => Ok($visitor.$visit_type(&$($ref)?local.0)),
+            Self::MemberSymbol(implementation) => Ok(implementation
+                .member_generic_arguments
+                .$accept_one_level($visitor)
+                && implementation
+                    .parent_generic_arguments
+                    .$accept_one_level($visitor)),
+        }
+    };
 }
 
 impl Element for Type {
@@ -178,32 +313,33 @@ impl Element for Type {
         &self,
         visitor: &mut impl Visitor,
     ) -> Result<bool, VisitNonApplicationTermError> {
-        match self {
-            Self::Primitive(_) | Self::Parameter(_) | Self::Inference(_) => {
-                Err(VisitNonApplicationTermError)
-            }
+        implements_type!(
+            self,
+            visitor,
+            visit_type,
+            visit_lifetime,
+            visit_constant,
+            accept_one_level
+        )
+    }
 
-            Self::Symbol(term) => {
-                if !term.generic_arguments.accept_one_level(visitor) {
-                    return Ok(false);
-                }
+    fn accept_single_mut(&mut self, visitor: &mut impl Visitor) -> bool {
+        visitor.visit_type_mut(self)
+    }
 
-                Ok(true)
-            }
-            Self::Pointer(term) => Ok(visitor.visit_type(&term.pointee)),
-            Self::Reference(term) => Ok(visitor.visit_lifetime(&term.lifetime)
-                && visitor.visit_type(&term.pointee)),
-            Self::Array(term) => Ok(visitor.visit_type(&term.r#type)
-                && visitor.visit_constant(&term.length)),
-            Self::Tuple(tuple) => Ok(tuple.accept_one_level(visitor)),
-            Self::Local(local) => Ok(visitor.visit_type(&local.0)),
-            Self::MemberSymbol(implementation) => Ok(implementation
-                .member_generic_arguments
-                .accept_one_level(visitor)
-                && implementation
-                    .parent_generic_arguments
-                    .accept_one_level(visitor)),
-        }
+    fn accept_one_level_mut(
+        &mut self,
+        visitor: &mut impl Visitor,
+    ) -> Result<bool, VisitNonApplicationTermError> {
+        implements_type!(
+            self,
+            visitor,
+            visit_type_mut,
+            visit_lifetime_mut,
+            visit_constant_mut,
+            accept_one_level_mut,
+            mut
+        )
     }
 }
 
@@ -218,6 +354,54 @@ impl Element for Lifetime {
     ) -> Result<bool, VisitNonApplicationTermError> {
         Err(VisitNonApplicationTermError)
     }
+
+    fn accept_single_mut(&mut self, visitor: &mut impl Visitor) -> bool {
+        visitor.visit_lifetime_mut(self)
+    }
+
+    fn accept_one_level_mut(
+        &mut self,
+        _: &mut impl Visitor,
+    ) -> Result<bool, VisitNonApplicationTermError> {
+        Err(VisitNonApplicationTermError)
+    }
+}
+
+macro_rules! implements_constant {
+    ($self:ident, $visitor:ident, $visit_type:ident, $visit_lifetime:ident, $visit_constant:ident, $accept_one_level:ident, $as_ref:ident, $iter:ident $(, $ref:ident)?) => {
+        match $self {
+            Self::Primitive(_) | Self::Parameter(_) | Self::Inference(_) => {
+                Err(VisitNonApplicationTermError)
+            }
+
+            Self::Struct(term) => {
+                for field in &$($ref)?term.fields {
+                    if !$visitor.$visit_constant(field) {
+                        return Ok(false);
+                    }
+                }
+
+                Ok(true)
+            }
+            Self::Enum(term) => Ok(term
+                .associated_value
+                .$as_ref()
+                .map_or(true, |x| $visitor.$visit_constant(x))),
+            Self::Array(term) => {
+                Ok(term.elements.$iter().all(|x| $visitor.$visit_constant(x)))
+            }
+
+            Self::Tuple(tuple) => Ok(tuple.$accept_one_level($visitor)),
+            Self::Local(local) => Ok($visitor.$visit_constant(&$($ref)?local.0)),
+            Self::Symbol(symbol) => {
+                Ok(symbol.generic_arguments.$accept_one_level($visitor))
+            }
+            Self::MemberSymbol(term) => {
+                Ok(term.member_generic_arguments.$accept_one_level($visitor)
+                    && term.parent_generic_arguments.$accept_one_level($visitor))
+            }
+        }
+    };
 }
 
 impl Element for Constant {
@@ -229,37 +413,36 @@ impl Element for Constant {
         &self,
         visitor: &mut impl Visitor,
     ) -> Result<bool, VisitNonApplicationTermError> {
-        match self {
-            Self::Parameter(_) | Self::Primitive(_) | Self::Inference(_) => {
-                Err(VisitNonApplicationTermError)
-            }
+        implements_constant!(
+            self,
+            visitor,
+            visit_type,
+            visit_lifetime,
+            visit_constant,
+            accept_one_level,
+            as_ref,
+            iter
+        )
+    }
 
-            Self::Struct(term) => {
-                for field in &term.fields {
-                    if !visitor.visit_constant(field) {
-                        return Ok(false);
-                    }
-                }
+    fn accept_single_mut(&mut self, visitor: &mut impl Visitor) -> bool {
+        visitor.visit_constant_mut(self)
+    }
 
-                Ok(true)
-            }
-            Self::Enum(term) => Ok(term
-                .associated_value
-                .as_ref()
-                .map_or(true, |x| visitor.visit_constant(x))),
-            Self::Array(term) => {
-                Ok(term.elements.iter().all(|x| visitor.visit_constant(x)))
-            }
-
-            Self::Local(local) => Ok(visitor.visit_constant(&local.0)),
-            Self::Tuple(tuple) => Ok(tuple.accept_one_level(visitor)),
-            Self::Symbol(symbol) => {
-                Ok(symbol.generic_arguments.accept_one_level(visitor))
-            }
-            Self::MemberSymbol(term) => {
-                Ok(term.member_generic_arguments.accept_one_level(visitor)
-                    && term.parent_generic_arguments.accept_one_level(visitor))
-            }
-        }
+    fn accept_one_level_mut(
+        &mut self,
+        visitor: &mut impl Visitor,
+    ) -> Result<bool, VisitNonApplicationTermError> {
+        implements_constant!(
+            self,
+            visitor,
+            visit_type_mut,
+            visit_lifetime_mut,
+            visit_constant_mut,
+            accept_one_level_mut,
+            as_mut,
+            iter_mut,
+            mut
+        )
     }
 }
