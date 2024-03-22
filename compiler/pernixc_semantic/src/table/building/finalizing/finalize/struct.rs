46a0d3c6e7becdf012af90564b5302f4deb65e67
Simmypeet <simmypeet@outlook.com>
2024-03-22 15:10:30 +0700

    Add basic struct finalization

--- compiler/pernixc_semantic/src/table/building/finalizing/finalize/struct.rs
+++ compiler/pernixc_semantic/src/table/building/finalizing/finalize/struct.rs
@@ -1,12 +1,12 @@
 use pernixc_base::diagnostic::Handler;
 use pernixc_syntax::syntax_tree;
 
-use super::{build_flag, Finalize};
+use super::{build_flag, Finalize, Occurrences};
 use crate::{
     arena::ID,
     error,
     symbol::Struct,
-    table::{building::finalizing::Finalizer, Table},
+    table::{building::finalizing::Finalizer, resolution::Config, Table},
 };
 
 build_flag! {
@@ -21,15 +21,42 @@ build_flag! {
 impl Finalize for Struct {
     type SyntaxTree = syntax_tree::item::Struct;
     type Flag = Flag;
-    type Data = ();
+    type Data = Occurrences;
 
     fn finalize(
-        _table: &Table<Finalizer>,
-        _symbol_id: ID<Self>,
-        _state_flag: Self::Flag,
-        _syntax_tree: &Self::SyntaxTree,
-        _data: &mut Self::Data,
-        _handler: &dyn Handler<Box<dyn error::Error>>,
+        table: &Table<Finalizer>,
+        symbol_id: ID<Self>,
+        state_flag: Self::Flag,
+        syntax_tree: &Self::SyntaxTree,
+        data: &mut Self::Data,
+        handler: &dyn Handler<Box<dyn error::Error>>,
     ) {
+        let resolution_config = Config {
+            ellided_lifetime_provider: None,
+            ellided_type_provider: None,
+            ellided_constant_provider: None,
+            observer: Some(data),
+            higher_ranked_liftimes: None,
+        };
+        match state_flag {
+            Flag::GenericParameter => table.create_generic_parameters(
+                symbol_id,
+                syntax_tree.signature().generic_parameters().as_ref(),
+                resolution_config,
+                handler,
+            ),
+            Flag::WhereClause => {
+                table.create_where_clause_predicates(
+                    symbol_id,
+                    syntax_tree.signature().where_clause().as_ref(),
+                    resolution_config,
+                    handler,
+                );
+            }
+            Flag::Body => { /*create struct fields*/ }
+            Flag::Check => {
+                table.check_occurrences(symbol_id.into(), data, handler);
+            }
+        }
     }
 }
