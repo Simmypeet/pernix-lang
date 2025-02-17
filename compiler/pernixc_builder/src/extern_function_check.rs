418178118eb6262bd69b068bcc734ea02d1fc826
Simmypeet <simmypeet@outlook.com>
2025-02-17 15:52:20 +0700

    add generic parameters check for extern functions

---
+++ compiler/pernixc_builder/src/extern_function_check.rs
@@ -0,0 +1,49 @@
+//! Contains the code related to checking the extern functions.
+
+use diagnostic::GenericParametersAreNotAllowedInExternFunction;
+use pernixc_abort::Abort;
+use pernixc_handler::Handler;
+use pernixc_table::{
+    component::Extern, diagnostic::Diagnostic, GlobalID, Table,
+};
+use pernixc_term::generic_parameter::GenericParameters;
+
+pub mod diagnostic;
+
+/// Checks wel-fromedness of the extern function.
+///
+/// For example, extern "C" functions are not allowed to have generic
+/// parameters.
+#[allow(clippy::missing_errors_doc)]
+pub fn extern_function_check(
+    table: &Table,
+    function_id: GlobalID,
+    handler: &dyn Handler<Box<dyn Diagnostic>>,
+) -> Result<(), Abort> {
+    let calling_conventation = *table.get::<Extern>(function_id);
+
+    match calling_conventation {
+        Extern::C => {
+            let generic_parameters =
+                table.query::<GenericParameters>(function_id)?;
+
+            let has_generic_parameters = !generic_parameters.types().is_empty()
+                || !generic_parameters.constants().is_empty();
+
+            if has_generic_parameters {
+                handler.receive(Box::new(
+                    GenericParametersAreNotAllowedInExternFunction {
+                        extern_function_id: function_id,
+                    },
+                ));
+            }
+        }
+
+        Extern::Unknown => todo!(),
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test;
