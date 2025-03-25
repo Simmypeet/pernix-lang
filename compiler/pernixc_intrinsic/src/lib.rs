be315ec0566051d82d3526cac157467995ab2dc6
Simmypeet <simmypeet@outlook.com>
2025-03-26 06:42:32 +0700

    make pointer and immutable ref copyable

--- compiler/pernixc_intrinsic/src/lib.rs
+++ compiler/pernixc_intrinsic/src/lib.rs
@@ -23,7 +23,7 @@ use pernixc_term::{
     },
     lifetime::Lifetime,
     predicate::{self, Outlives},
-    r#type::{Primitive, Qualifier, Reference, Type},
+    r#type::{Pointer, Primitive, Qualifier, Reference, Type},
     where_clause::{Predicate, WhereClause},
     Default,
 };
@@ -59,10 +59,20 @@ implements Copy[isize]
 implements Copy[usize]
 
 // mutable reference can't be copied
-implements['a, T] Copy[&'a mutable T] delete
+implements['a, T] Copy[&'a mut T] delete:
     where:
         T: 'a
-*/
+
+implements['a, T] Copy[&'a T]:
+    where:
+        T: 'a
+
+implements[T] Copy[*T]:
+    pass
+
+implements[T] Copy[*mut T]:
+    pass
+ */
 
 /*
 // Drop trait synopsis:
@@ -239,7 +249,7 @@ fn initialize_alignof(
     }));
 }
 
-#[allow(clippy::too_many_lines)]
+#[allow(clippy::too_many_lines, clippy::cognitive_complexity)]
 fn initialize_copy_marker(
     table: &Table,
     id_gen: &mut impl Iterator<Item = usize>,
@@ -356,6 +366,100 @@ fn initialize_copy_marker(
 
         implemented.insert(impl_id);
     }
+
+    // immutable reference of any type can be copied
+    {
+        let impl_id = GlobalID::new(
+            TargetID::CORE,
+            pernixc_table::ID(id_gen.next().unwrap()),
+        );
+        let mut generic_parameters = GenericParameters::default();
+        let a_lt = Lifetime::Parameter(LifetimeParameterID::new(
+            impl_id,
+            generic_parameters
+                .add_lifetime_parameter(LifetimeParameter {
+                    name: "a".to_string(),
+                    span: None,
+                })
+                .unwrap(),
+        ));
+        let t_ty = Type::Parameter(TypeParameterID::new(
+            impl_id,
+            generic_parameters
+                .add_type_parameter(TypeParameter {
+                    name: "T".to_string(),
+                    span: None,
+                })
+                .unwrap(),
+        ));
+
+        assert!(table
+            .add_component(impl_id, SymbolKind::PositiveMarkerImplementation));
+        assert!(table.add_component(impl_id, generic_parameters));
+        assert!(table.add_component(impl_id, Implements(copy_marker_id)));
+        assert!(table.add_component(impl_id, ForallLifetimes::default()));
+        assert!(table.add_component(impl_id, {
+            let mut where_clause = WhereClause::default();
+            where_clause.predicates.push(Predicate {
+                predicate: predicate::Predicate::TypeOutlives(Outlives::new(
+                    t_ty.clone(),
+                    a_lt,
+                )),
+                span: None,
+            });
+            where_clause
+        }));
+        assert!(table.add_component(impl_id, {
+            let mut implementation = Implementation::default();
+            implementation.generic_arguments.types.push(Type::Reference(
+                Reference {
+                    qualifier: Qualifier::Immutable,
+                    lifetime: a_lt,
+                    pointee: Box::new(t_ty),
+                },
+            ));
+            implementation
+        }));
+
+        implemented.insert(impl_id);
+    }
+
+    // mutable/immutable pointer of any type can be copied
+    let mut impl_pointer = |mutable| {
+        let impl_id = GlobalID::new(
+            TargetID::CORE,
+            pernixc_table::ID(id_gen.next().unwrap()),
+        );
+        let mut generic_parameters = GenericParameters::default();
+        let t_ty = Type::Parameter(TypeParameterID::new(
+            impl_id,
+            generic_parameters
+                .add_type_parameter(TypeParameter {
+                    name: "T".to_string(),
+                    span: None,
+                })
+                .unwrap(),
+        ));
+
+        assert!(table
+            .add_component(impl_id, SymbolKind::PositiveMarkerImplementation));
+        assert!(table.add_component(impl_id, generic_parameters));
+        assert!(table.add_component(impl_id, Implements(copy_marker_id)));
+        assert!(table.add_component(impl_id, ForallLifetimes::default()));
+        assert!(table.add_component(impl_id, WhereClause::default()));
+        assert!(table.add_component(impl_id, {
+            let mut implementation = Implementation::default();
+            implementation.generic_arguments.types.push(Type::Pointer(
+                Pointer { mutable, pointee: Box::new(t_ty) },
+            ));
+            implementation
+        }));
+
+        implemented.insert(impl_id);
+    };
+
+    impl_pointer(false);
+    impl_pointer(true);
 
     assert!(table.add_component(copy_marker_id, implemented));
 }
