f020cc79e869a65c2c0774c9cf52568a08a87d00
Simmypeet <simmypeet@outlook.com>
2025-04-03 21:13:14 +0700

    refactor arena

--- compiler/pernixc_arena/src/lib.rs
+++ compiler/pernixc_arena/src/lib.rs
@@ -5,7 +5,6 @@
 //! more type safety when working with various containers of different types.
 
 use std::{
-    borrow::Borrow,
     collections::{hash_map::Entry, HashMap},
     fmt::Debug,
     hash::Hash,
@@ -117,84 +116,16 @@ impl<'de, T: 'static> serde::Deserialize<'de> for ID<T> {
     }
 }
 
-/// A helper struct that allows reserving [`ID`]s in an [`Arena`].
-///
-/// These [`ID`]s are guaranteed to be unique to the [`Arena`] and to the ones
-/// reserved by this struct. These [`ID`]s can then be used to insert items into
-/// the [`Arena`] with the [`Arena::insert_with_id`] method.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Reserve<'a, T: 'static, I = T> {
-    arena: &'a Arena<T>,
-    reserved: HashMap<ID<T>, I>,
-}
-
-impl<'a, T: 'static, I> Reserve<'a, T, I> {
-    /// Creates a new [`Reserve`] struct that can be used to reserve [`ID`]s in
-    /// the given [`Arena`].
-    #[must_use]
-    pub fn new(arena: &'a Arena<T>) -> Self {
-        Self { arena, reserved: HashMap::new() }
-    }
-
-    /// Reserves a new [`ID`] in the [`Arena`] and returns it.
-    ///
-    /// The [`ID`] is guaranteed to be unique to the [`Arena`] and the ones
-    /// reserved by this struct.
-    pub fn reserve(&mut self, information: I) -> ID<T> {
-        let mut id = self.arena.get_available_id().into_index();
-
-        let entry = loop {
-            let idx = ID::from_index(id);
-            if let Entry::Vacant(entry) = self.reserved.entry(idx) {
-                if !self.arena.items.contains_key(&idx) {
-                    break entry;
-                }
-            }
-
-            id += 1;
-        };
-
-        entry.insert(information);
-
-        ID::from_index(id)
-    }
-
-    /// Gets the information associated with the reserved [`ID`].
-    #[must_use]
-    pub fn get_reserved(&self, id: ID<T>) -> Option<&I> {
-        self.reserved.get(&id)
-    }
-
-    /// Returns the map of reserved [`ID`]s and their associated information.
-    #[must_use]
-    pub fn into_reserved(self) -> HashMap<ID<T>, I> { self.reserved }
-
-    /// Returns the number of reserved [`ID`]s (not the total number of items in
-    /// the [`Arena`]).
-    #[must_use]
-    pub fn len(&self) -> usize { self.reserved.len() }
-
-    /// Returns `true` if there are no reserved [`ID`]s.
-    #[must_use]
-    pub fn is_empty(&self) -> bool { self.reserved.is_empty() }
-}
-
 /// Represents a collection of items of type `T` that can be referenced by an
 /// [`ID`].
-///
-/// Internally, all the items are stored in a [`Vec`], and the [`ID`] is just an
-/// index to the item in the [`Vec`]. However, unlike [`Vec`], [`Arena`] doesn't
-/// allow removing items in between since it will invalidate all the [`ID`]s
-/// given out before. This data structure is commonly used in graph structures
-/// where the nodes are stored in an [`Arena`] and the edges are represented by
-/// [`ID`]s.
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Arena<T, Idx: Key = ID<T>> {
+    last_gen: usize,
     items: HashMap<Idx, T>,
 }
 
 impl<T, Idx: Key> Default for Arena<T, Idx> {
-    fn default() -> Self { Self { items: HashMap::new() } }
+    fn default() -> Self { Self { items: HashMap::new(), last_gen: 0 } }
 }
 
 impl<T, Idx: Key> Arena<T, Idx> {
@@ -210,11 +141,16 @@ impl<T, Idx: Key> Arena<T, Idx> {
     #[must_use]
     pub fn is_empty(&self) -> bool { self.items.is_empty() }
 
+    /// Returns the next available [`Idx`] in the [`Arena`].
+    #[must_use]
+    pub fn next_id(&self) -> Idx { Key::from_index(self.last_gen) }
+
     /// Inserts a new item into the [`Arena`] and returns its `Idx`.
     pub fn insert(&mut self, item: T) -> Idx {
-        let next_id = self.get_available_id();
+        let next_id = self.next_id();
 
         assert!(self.items.insert(next_id, item).is_none()); // no prior value
+        self.last_gen += 1;
 
         next_id
     }
@@ -222,11 +158,12 @@ impl<T, Idx: Key> Arena<T, Idx> {
     /// Inserts a new item into the [`Arena`] by constructing it with the given
     /// function with the ID of the item as the argument.
     pub fn insert_with(&mut self, f: impl FnOnce(Idx) -> T) -> Idx {
-        let id = self.get_available_id();
+        let next_id = self.next_id();
 
-        assert!(self.items.insert(id, f(id)).is_none()); // no prior value
+        assert!(self.items.insert(next_id, f(next_id)).is_none()); // no prior value
+        self.last_gen += 1;
 
-        id
+        next_id
     }
 
     /// Retains only the items in the [`Arena`] that satisfy the given
@@ -251,6 +188,7 @@ impl<T, Idx: Key> Arena<T, Idx> {
             Entry::Occupied(_) => Err(item),
             Entry::Vacant(entry) => {
                 entry.insert(item);
+                self.last_gen = std::cmp::max(self.last_gen, id.into_index());
 
                 Ok(())
             }
@@ -267,25 +205,13 @@ impl<T, Idx: Key> Arena<T, Idx> {
             .map(|(id, item)| (ID::from_index(id.into_index()), f(item)))
             .collect();
 
-        Arena { items }
-    }
-
-    /// Gets the [`ID`] that would be assigned to the next item inserted into
-    /// the [`Arena`].
-    #[must_use]
-    pub fn get_available_id(&self) -> Idx {
-        let mut index = self.items.len();
-
-        while self.items.contains_key(&Idx::from_index(index)) {
-            index += 1;
-        }
-
-        Idx::from_index(index)
+        Arena { items, last_gen: self.last_gen }
     }
 
     /// Returns a reference to the item in the [`Arena`] with the given `Idx`.
     #[must_use]
     pub fn get(&self, id: Idx) -> Option<&T> { self.items.get(&id) }
+
     /// Returns a mutable reference to the item in the [`Arena`] with the given
     /// `Idx`.
     #[must_use]
@@ -348,8 +274,9 @@ impl<T, Idx: Key> IntoIterator for Arena<T, Idx> {
 
 impl<T, Idx: Key> FromIterator<(Idx, T)> for Arena<T, Idx> {
     fn from_iter<U: IntoIterator<Item = (Idx, T)>>(iter: U) -> Self {
-        let items = iter.into_iter().collect();
-        Self { items }
+        let items: HashMap<Idx, T> = iter.into_iter().collect();
+        let max = items.keys().copied().max().unwrap_or(Idx::from_index(0));
+        Self { items, last_gen: max.into_index() }
     }
 }
 
@@ -365,138 +292,4 @@ impl<'a, T, Idx: Key> IntoIterator for &'a mut Arena<T, Idx> {
     type Item = (&'a Idx, &'a mut T);
 
     fn into_iter(self) -> Self::IntoIter { self.items.iter_mut() }
-}
-
-/// Represents a map from that allows referencing items in the [`Map`] by either
-/// their `K` key or their [`ID`].
-///
-/// Accessing the items by their [`ID`] is more efficient than accessing them by
-/// their `K` key since the former is just an index to the item in the
-/// [`Arena`], while the latter requires a hash map lookup.
-#[derive(
-    Debug, Clone, PartialEq, Eq, derive_more::Index, derive_more::IndexMut,
-)]
-pub struct Map<T, Secondary: Hash + Eq = String, Primary: Key = ID<T>> {
-    #[index]
-    #[index_mut]
-    arena: Arena<T, Primary>,
-
-    items: HashMap<Secondary, Primary>,
-}
-
-impl<T, Secondary: Hash + Eq, Primary: Key> Map<T, Secondary, Primary> {
-    /// Creates a new empty [`Map`].
-    #[must_use]
-    pub fn new() -> Self { Self { arena: Arena::new(), items: HashMap::new() } }
-
-    /// Returns the number of items in the [`Map`].
-    #[must_use]
-    pub fn len(&self) -> usize { self.arena.len() }
-
-    /// Returns `true` if the [`Map`] contains no items.
-    #[must_use]
-    pub fn is_empty(&self) -> bool { self.arena.is_empty() }
-
-    /// Inserts a new item into the [`Map`] with the given key
-    ///
-    /// # Returns
-    ///
-    /// Returns `Ok` with the [`ID`] of the new item if the key doesn't exist in
-    /// the [`Map`].
-    ///
-    /// # Errors
-    ///
-    /// Returns `Err` with a tuple of the [`ID`] of the existing item and the
-    /// new item if the key already exists in the [`Map`].
-    pub fn insert(
-        &mut self,
-        key: Secondary,
-        item: T,
-    ) -> Result<Primary, (Primary, T)> {
-        match self.items.entry(key) {
-            Entry::Occupied(entry) => {
-                let id = *entry.get();
-                Err((id, item))
-            }
-            Entry::Vacant(entry) => {
-                let id = self.arena.insert(item);
-                entry.insert(id);
-                Ok(id)
-            }
-        }
-    }
-
-    /// Returns a reference to the item in the [`Map`] with the given key.
-    ///
-    /// # Errors
-    ///
-    /// Returns `Err` if the key doesn't exist in the [`Map`].
-    pub fn get_id<Q: ?Sized + Hash + Eq>(&self, key: &Q) -> Option<Primary>
-    where
-        Secondary: Borrow<Q>,
-    {
-        self.items.get(key).copied()
-    }
-
-    /// Returns a reference to the item in the [`Map`] with the given [`ID`].
-    #[must_use]
-    pub fn get(&self, id: Primary) -> Option<&T> { self.arena.get(id) }
-
-    /// Returns a mutable reference to the item in the [`Map`] with the given
-    /// [`ID`].
-    #[must_use]
-    pub fn get_mut(&mut self, id: Primary) -> Option<&mut T> {
-        self.arena.get_mut(id)
-    }
-
-    /// Returns an iteartor over the items and their primary keys in the
-    /// [`Map`].
-    #[must_use]
-    pub fn iter_primary(&self) -> impl ExactSizeIterator<Item = (Primary, &T)> {
-        self.arena.iter()
-    }
-
-    /// Returns an iterator over the items and their secondary keys in the
-    /// [`Map`].
-    ///
-    /// The order of the items is **not** maintained.
-    #[must_use]
-    pub fn iter(&self) -> impl ExactSizeIterator<Item = (&Secondary, &T)> {
-        self.items.iter().map(|(k, v)| (k, self.get(*v).unwrap()))
-    }
-
-    /// Returns an iterator over the primary keys in the [`Map`].
-    ///
-    /// The order of the keys is **not** maintained.
-    #[must_use]
-    pub fn ids(&self) -> impl ExactSizeIterator<Item = Primary> + '_ {
-        self.arena.ids()
-    }
-
-    /// Returns an iterator over the secondary keys in the [`Map`].
-    ///
-    /// The order of the keys is **not** maintained.
-    #[must_use]
-    pub fn secondaries(&self) -> impl ExactSizeIterator<Item = &Secondary> {
-        self.items.keys()
-    }
-
-    /// Returns an iterator over the values in the [`Map`].
-    ///
-    /// The order of the values is maintained.
-    #[must_use]
-    pub fn values(&self) -> impl ExactSizeIterator<Item = &T> {
-        self.arena.items()
-    }
-
-    /// Returns an mutable iterator over the items in the [`Map`].
-    ///
-    /// The order of the values is not maintained.
-    pub fn values_mut(&mut self) -> impl ExactSizeIterator<Item = &mut T> {
-        self.arena.items_mut()
-    }
-}
-
-impl<T, K: Eq + Hash, Idx: Key> Default for Map<T, K, Idx> {
-    fn default() -> Self { Self::new() }
 }
