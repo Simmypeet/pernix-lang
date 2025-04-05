93b37097d9228f285ce7240d6a3e185d553c890a
Simmypeet <simmypeet@outlook.com>
2025-04-06 02:40:40 +0700

    update pernixc_diagnostic

--- compiler/pernixc_diagnostic/src/lib.rs
+++ compiler/pernixc_diagnostic/src/lib.rs
@@ -1,22 +1,33 @@
 //! Contains the definition of the [`Diagnostic`] struct and related types.
 
-use pernixc_log::{Message, Severity, SourceCodeDisplay};
-use pernixc_source_file::Span;
-
 /// Implement this trait for a type that can report a diagnostic.
 ///
 /// This trait is typically implemented by error and warning types.
-pub trait Report<Param> {
+pub trait Report<Param, S> {
     /// Creates a diagnostic.
-    fn report(&self, parameter: Param) -> Diagnostic;
+    fn report(&self, parameter: Param) -> Diagnostic<S>;
+}
+
+/// Enumeration of the severity levels of a diagnostic.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Severity {
+    /// An error that prevents the program from compiling.
+    Error,
+
+    /// A warning that does not prevent the program from compiling.
+    Warning,
+
+    /// An informational message that does not prevent the program from
+    /// compiling.
+    Info,
 }
 
 /// A struct containing all the information required to display the diagnostic
 /// to the user.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Diagnostic {
+pub struct Diagnostic<S> {
     /// The span location where the diagnostic occurred.
-    pub span: Span,
+    pub span: S,
 
     /// The message to display to the user.
     pub message: String,
@@ -32,35 +43,15 @@ pub struct Diagnostic {
     ///
     /// For example, for unimplemented methods, this could be a list of
     /// methods that need to be implemented.
-    pub related: Vec<Related>,
+    pub related: Vec<Related<S>>,
 }
 
 /// The related information that is displayed alongside the main [`Diagnostic`].
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Related {
+pub struct Related<S> {
     /// The span location to display the message.
-    pub span: Span,
+    pub span: S,
 
     /// The message to display to the user.
     pub message: String,
-}
-
-impl std::fmt::Display for Diagnostic {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}\n{}",
-            Message::new(self.severity, &self.message),
-            SourceCodeDisplay::new(&self.span, self.help_message.as_deref())
-        )?;
-        for related in &self.related {
-            write!(
-                f,
-                "\n{}",
-                SourceCodeDisplay::new(&related.span, Some(&related.message))
-            )?;
-        }
-
-        Ok(())
-    }
 }
