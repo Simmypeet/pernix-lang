8dde3bfbec69043f87d8ec1b96bd665dd8670972
Simmypeet <simmypeet@outlook.com>
2025-04-07 17:26:42 +0700

    Input trait accepts additional generic parameter

--- compiler/pernixc_test_input/src/lib.rs
+++ compiler/pernixc_test_input/src/lib.rs
@@ -9,28 +9,28 @@ use proptest::{
 };
 
 /// Represents an input generated for testing purposes.
-pub trait Input<Output: Debug>: Debug {
+pub trait Input<Output: Debug, Parameters>: Debug {
     /// Verifies that the given output complies with this input.
     ///
     /// # Errors
     /// [`proptest::test_runner::TestCaseError`]: for any reason the assertion fails.
-    fn assert(self, output: Output) -> TestCaseResult;
+    fn assert(self, output: Output, parameters: Parameters) -> TestCaseResult;
 }
 
-impl<T: Debug, U> Input<&Box<T>> for &Box<U>
+impl<T: Debug, U, P> Input<&Box<T>, P> for &Box<U>
 where
-    for<'a, 'b> &'a U: Input<&'b T>,
+    for<'a, 'b> &'a U: Input<&'b T, P>,
     Self: Debug,
 {
-    fn assert(self, output: &Box<T>) -> TestCaseResult {
-        self.as_ref().assert(output.as_ref())
+    fn assert(self, output: &Box<T>, parameters: P) -> TestCaseResult {
+        self.as_ref().assert(output.as_ref(), parameters)
     }
 }
 
-impl<T: Debug, U: Debug + Input<T>> Input<Option<T>> for Option<U> {
-    fn assert(self, output: Option<T>) -> TestCaseResult {
+impl<T: Debug, U: Debug + Input<T, P>, P> Input<Option<T>, P> for Option<U> {
+    fn assert(self, output: Option<T>, parameters: P) -> TestCaseResult {
         match (self, output) {
-            (Some(input), Some(output)) => input.assert(output),
+            (Some(input), Some(output)) => input.assert(output, parameters),
             (None, None) => Ok(()),
             (input, output) => Err(TestCaseError::fail(format!(
                 "expected {input:?}, got {output:?}"
@@ -39,29 +39,30 @@ impl<T: Debug, U: Debug + Input<T>> Input<Option<T>> for Option<U> {
     }
 }
 
-impl<T: Debug, U: Debug> Input<&[T]> for &[U]
+impl<T: Debug, U: Debug, P: Clone> Input<&[T], P> for &[U]
 where
-    for<'a, 'b> &'a U: Input<&'b T>,
+    for<'a, 'b> &'a U: Input<&'b T, P>,
 {
-    fn assert(self, output: &[T]) -> TestCaseResult {
+    fn assert(self, output: &[T], parameters: P) -> TestCaseResult {
         prop_assert_eq!(self.len(), output.len());
 
         for (input, output) in self.iter().zip(output.iter()) {
-            input.assert(output)?;
+            input.assert(output, parameters.clone())?;
         }
 
         Ok(())
     }
 }
 
-impl<T: Debug, U: Debug, V: Debug, W: Debug> Input<&(T, U)> for &(V, W)
+impl<T: Debug, U: Debug, V: Debug, W: Debug, P: Clone> Input<&(T, U), P>
+    for &(V, W)
 where
-    for<'a, 'b> &'a V: Input<&'b T>,
-    for<'a, 'b> &'a W: Input<&'b U>,
+    for<'a, 'b> &'a V: Input<&'b T, P>,
+    for<'a, 'b> &'a W: Input<&'b U, P>,
 {
-    fn assert(self, output: &(T, U)) -> TestCaseResult {
-        self.0.assert(&output.0)?;
-        self.1.assert(&output.1)?;
+    fn assert(self, output: &(T, U), parameters: P) -> TestCaseResult {
+        self.0.assert(&output.0, parameters.clone())?;
+        self.1.assert(&output.1, parameters)?;
 
         Ok(())
     }
