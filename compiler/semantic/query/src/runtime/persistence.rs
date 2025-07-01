be202ca902d7d0c6b1d20ba54238fa7efb847fcf
Simmypeet <simmypeet@outlook.com>
2025-07-02 02:07:25 +0700

    add save dependency graph

--- compiler/semantic/query/src/runtime/persistence.rs
+++ compiler/semantic/query/src/runtime/persistence.rs
@@ -12,6 +12,7 @@ use std::{
 use getset::Getters;
 use ouroboros::self_referencing;
 use parking_lot::{RwLock, RwLockUpgradableReadGuard};
+use pernixc_arena::state::Hash;
 use pernixc_hash::HashSet;
 use pernixc_serialize::{
     binary::{de::BinaryDeserializer, ser::BinarySerializer},
@@ -104,11 +105,10 @@ pub struct Persistence {
         &mut BinaryDeserializer<Box<dyn ReadAny>>,
     ) -> Result<VersionInfo, std::io::Error>,
 
-    serialize_call_graph: fn(
-        &QueryTracker,
+    serialize_dependency_graph: fn(
+        &HashSet<DynamicBox>,
+        &mut BinarySerializer<Box<dyn WriteAny>>,
         &dyn Any,
-        &redb::Database,
-        &redb::Database,
     ) -> Result<(), std::io::Error>,
 }
 
@@ -314,7 +314,7 @@ impl Persistence {
             deserialize_dependencies,
             deserialize_version_info,
 
-            serialize_call_graph: serialize_call_graph::<E>,
+            serialize_dependency_graph: serialize_dependency_graph::<E>,
         })
     }
 
@@ -347,19 +347,6 @@ impl Persistence {
     /// Registers a key to be skipped during serialization.
     pub fn register_skip_key<K: Key>(&mut self) {
         self.skip_keys.insert(K::STABLE_TYPE_ID);
-    }
-
-    /// Serializes thne entire map to the persistence storage.
-    pub fn serialize_call_graph(
-        &self,
-        query_tracker: &QueryTracker,
-    ) -> Result<(), std::io::Error> {
-        (self.serialize_call_graph)(
-            query_tracker,
-            self.serde_extension.as_ref(),
-            &self.dependency_graph_database,
-            &self.version_info_database,
-        )
     }
 
     /// Commits all the saved changes made by various `save_*` methods to the
@@ -503,6 +490,61 @@ impl Persistence {
 
             value
         })
+    }
+
+    /// Saves the version information for a given key fingerprint to the
+    /// persistence storage.
+    #[allow(clippy::mutable_key_type)]
+    pub fn save_dependency_graph(
+        &self,
+        stable_type_id: StableTypeID,
+        fingerprint: u128,
+        dependencies: &HashSet<DynamicBox>,
+    ) -> Result<(), std::io::Error> {
+        let buffer = BUFFER.with(|b| std::mem::take(&mut *b.borrow_mut()));
+
+        let mut binary_serializer =
+            BinarySerializer::<Box<dyn WriteAny>>::new(Box::new(buffer));
+
+        (self.serialize_dependency_graph)(
+            dependencies,
+            &mut binary_serializer,
+            self.serde_extension.as_ref(),
+        )?;
+
+        let box_any: Box<dyn Any> = binary_serializer.into_inner();
+        let mut buffer =
+            *box_any.downcast::<Vec<u8>>().expect("Buffer must be a Vec<u8>");
+
+        let mut tx = self.dependency_graph_write_transaction.upgradable_read();
+        Self::ensure_transaction(&self.dependency_graph_database, &mut tx)?;
+
+        tx.with_upgraded(|tx| {
+            tx.as_mut().unwrap().with_table_mut(
+                |table| -> Result<(), std::io::Error> {
+                    table
+                        .insert(
+                            (stable_type_id.as_u128(), fingerprint),
+                            buffer.as_slice(),
+                        )
+                        .map_err(|e| {
+                            std::io::Error::other(format!(
+                                "Failed to insert entry into table: {e}",
+                            ))
+                        })?;
+
+                    Ok(())
+                },
+            )
+        })?;
+
+        BUFFER.with(|b| {
+            // clear the buffer but keep the allocated memory
+            buffer.clear();
+            *b.borrow_mut() = buffer;
+        });
+
+        Ok(())
     }
 
     /// Saves the version information for a given key fingerprint to the
@@ -669,194 +711,23 @@ impl Persistence {
     }
 }
 
-#[allow(clippy::too_many_lines)]
-fn serialize_call_graph<
+#[allow(clippy::mutable_key_type)]
+fn serialize_dependency_graph<
     E: DynamicSerialize<BinarySerializer<Box<dyn WriteAny>>>
         + DynamicDeserialize<BinaryDeserializer<Box<dyn ReadAny>>>
         + Send
         + Sync
         + 'static,
 >(
-    query_tracker: &QueryTracker,
+    dependency_graph: &HashSet<DynamicBox>,
+    serializer: &mut BinarySerializer<Box<dyn WriteAny>>,
     serde_extension: &dyn Any,
-    dependency_graph_database: &redb::Database,
-    version_info_database: &redb::Database,
 ) -> Result<(), std::io::Error> {
     let extension = serde_extension
         .downcast_ref::<E>()
         .expect("serde_extension must match the expected type");
 
-    let mut dependency_graph = Ok(());
-    let mut version_info = Ok(());
-
-    rayon::scope(|s| {
-        s.spawn(|_| {
-            let _span = trace_span!("serialize dependency graph").entered();
-
-            let tx =
-                match dependency_graph_database.begin_write().map_err(|e| {
-                    std::io::Error::other(format!(
-                        "Failed to begin write transaction: {e}",
-                    ))
-                }) {
-                    Ok(tx) => tx,
-                    Err(e) => {
-                        dependency_graph = Err(e);
-                        return;
-                    }
-                };
-
-            let table = RwLock::new(
-                match tx.open_table(TABLE).map_err(|e| {
-                    std::io::Error::other(format!("Failed to open table: {e}",))
-                }) {
-                    Ok(table) => table,
-                    Err(e) => {
-                        dependency_graph = Err(e);
-                        return;
-                    }
-                },
-            );
-
-            let result = query_tracker
-                .dependency_graph()
-                .par_iter()
-                .map(|(key, dependencies)| {
-                    let buffer =
-                        BUFFER.with(|b| std::mem::take(&mut *b.borrow_mut()));
-
-                    let mut serializer =
-                        BinarySerializer::<Box<dyn WriteAny>>::new(Box::new(
-                            buffer,
-                        ));
-
-                    dependencies.serialize(&mut serializer, extension)?;
-
-                    let any_box: Box<dyn Any> = serializer.into_inner();
-                    let mut buffer = any_box.downcast::<Vec<u8>>().unwrap();
-
-                    table
-                        .write()
-                        .insert(
-                            (key.stable_type_id().as_u128(), key.fingerprint()),
-                            buffer.as_slice(),
-                        )
-                        .map_err(|e| {
-                            std::io::Error::other(format!(
-                                "Failed to insert entry into table: {e}",
-                            ))
-                        })?;
-
-                    BUFFER.with(|b| {
-                        // clear the buffer but keep the allocated
-                        // memory
-                        buffer.clear();
-                        *b.borrow_mut() = *buffer;
-                    });
-
-                    Ok(())
-                })
-                .collect::<Result<(), std::io::Error>>();
-
-            if let Err(e) = result {
-                dependency_graph = Err(e);
-                return;
-            }
-
-            drop(table);
-
-            dependency_graph = tx.commit().map_err(|e| {
-                std::io::Error::other(format!(
-                    "Failed to commit transaction: {e}",
-                ))
-            });
-        });
-
-        s.spawn(|_| {
-            let _span = trace_span!("serialize version info").entered();
-
-            let tx = match version_info_database.begin_write().map_err(|e| {
-                std::io::Error::other(format!(
-                    "Failed to begin write transaction: {e}",
-                ))
-            }) {
-                Ok(tx) => tx,
-                Err(e) => {
-                    version_info = Err(e);
-                    return;
-                }
-            };
-
-            let table = RwLock::new(
-                match tx.open_table(TABLE).map_err(|e| {
-                    std::io::Error::other(format!("Failed to open table: {e}",))
-                }) {
-                    Ok(table) => table,
-                    Err(e) => {
-                        version_info = Err(e);
-                        return;
-                    }
-                },
-            );
-
-            let result = query_tracker
-                .version_info_by_keys()
-                .par_iter()
-                .map(|(key, version_info)| {
-                    let buffer =
-                        BUFFER.with(|b| std::mem::take(&mut *b.borrow_mut()));
-                    let mut serializer =
-                        BinarySerializer::<Box<dyn WriteAny>>::new(Box::new(
-                            buffer,
-                        ));
-
-                    version_info.serialize(&mut serializer, extension)?;
-
-                    let any_box: Box<dyn Any> = serializer.into_inner();
-                    let mut buffer = any_box.downcast::<Vec<u8>>().unwrap();
-
-                    table
-                        .write()
-                        .insert(
-                            (key.stable_type_id().as_u128(), key.fingerprint()),
-                            buffer.as_slice(),
-                        )
-                        .map_err(|e| {
-                            std::io::Error::other(format!(
-                                "Failed to insert entry into table: {e}",
-                            ))
-                        })?;
-
-                    BUFFER.with(|b| {
-                        // clear the buffer but keep the allocated
-                        // memory
-                        buffer.clear();
-                        *b.borrow_mut() = *buffer;
-                    });
-
-                    Ok(())
-                })
-                .collect::<Result<(), std::io::Error>>();
-
-            if let Err(e) = result {
-                version_info = Err(e);
-                return;
-            }
-
-            drop(table);
-
-            version_info = tx.commit().map_err(|e| {
-                std::io::Error::other(format!(
-                    "Failed to commit transaction: {e}",
-                ))
-            });
-        });
-    });
-
-    dependency_graph?;
-    version_info?;
-
-    Ok(())
+    dependency_graph.serialize(serializer, extension)
 }
 
 impl Drop for Persistence {
