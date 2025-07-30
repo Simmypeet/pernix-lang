25fbf501646652cb5ded7876dd2a80b2029e8f25
Simmypeet <simmypeet@outlook.com>
2025-07-30 21:07:35 +0700

    add term visitor

--- compiler/semantic/term/src/lib.rs
+++ compiler/semantic/term/src/lib.rs
@@ -13,6 +13,7 @@ pub mod matching;
 pub mod sub_term;
 pub mod tuple;
 pub mod r#type;
+pub mod visitor;
 
 /// Represents a type that can never be instantiated.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -36,4 +37,25 @@ pub enum Term {
     Constant(constant::Constant),
     Lifetime(lifetime::Lifetime),
     Type(r#type::Type),
+}
+
+/// A reference to a term, which can be either a constant, lifetime, or type.
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, derive_more::From,
+)]
+#[allow(missing_docs)]
+pub enum TermRef<'a> {
+    Constant(&'a constant::Constant),
+    Lifetime(&'a lifetime::Lifetime),
+    Type(&'a r#type::Type),
+}
+
+/// A mutable reference to a term, which can be either a constant, lifetime, or
+/// type.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, derive_more::From)]
+#[allow(missing_docs)]
+pub enum TermMut<'a> {
+    Constant(&'a mut constant::Constant),
+    Lifetime(&'a mut lifetime::Lifetime),
+    Type(&'a mut r#type::Type),
 }
