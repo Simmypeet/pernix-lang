bd2a0f8ed8981554695b5812fc173f685e823a00
Simmypeet <simmypeet@outlook.com>
2025-07-31 17:45:30 +0700

    add global arbitrary

--- compiler/base/target/src/arbitrary.rs
+++ compiler/base/target/src/arbitrary.rs
@@ -5,7 +5,7 @@ use proptest::{
     prop_oneof,
 };
 
-use crate::TargetID;
+use crate::{Global, TargetID};
 
 impl Arbitrary for TargetID {
     type Parameters = ();
@@ -18,5 +18,18 @@ impl Arbitrary for TargetID {
             4 => u64::arbitrary().prop_map(Self::Extern),
         ]
         .boxed()
+    }
+}
+
+impl<ID: Arbitrary<Strategy = BoxedStrategy<ID>> + 'static> Arbitrary
+    for Global<ID>
+{
+    type Parameters = Option<BoxedStrategy<ID>>;
+    type Strategy = BoxedStrategy<Self>;
+
+    fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
+        (TargetID::arbitrary_with(()), args.unwrap_or_else(ID::arbitrary))
+            .prop_map(|(target, id)| target.make_global(id))
+            .boxed()
     }
 }
