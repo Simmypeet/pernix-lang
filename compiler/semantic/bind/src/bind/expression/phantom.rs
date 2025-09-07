ff3b68471d0a833e63525ad74088dd14ebf2e339
Simmypeet <simmypeet@outlook.com>
2025-09-07 17:54:10 +0700

    fix incorrect type bug

--- compiler/semantic/bind/src/bind/expression/phantom.rs
+++ compiler/semantic/bind/src/bind/expression/phantom.rs
@@ -4,7 +4,7 @@ use pernixc_ir::value::{
     Value,
 };
 use pernixc_source_file::SourceElement;
-use pernixc_term::r#type::Type;
+use pernixc_term::r#type::{self, Type};
 
 use crate::{
     bind::{Bind, Config, Expression},
@@ -22,9 +22,9 @@ impl Bind<&pernixc_syntax::expression::unit::Phantom>
         _: &dyn Handler<crate::diagnostic::Diagnostic>,
     ) -> Result<Expression, Error> {
         Ok(Expression::RValue(Value::Literal(Literal::Phantom(Phantom {
-            r#type: Type::Inference(
+            r#type: Type::Phantom(r#type::Phantom(Box::new(Type::Inference(
                 self.create_type_inference(constraint::Type::All(false)),
-            ),
+            )))),
             span: Some(syntax_tree.span()),
         }))))
     }
