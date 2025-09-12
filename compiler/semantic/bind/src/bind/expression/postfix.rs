35c6c8b91a05332c43a9c8bc5090d4ea5bd7456d
Simmypeet <simmypeet@outlook.com>
2025-09-12 13:42:52 +0700

    move to correct file

--- compiler/semantic/bind/src/bind/expression/postfix.rs
+++ compiler/semantic/bind/src/bind/expression/postfix.rs
@@ -1,1 +1,47 @@
+use pernixc_handler::Handler;
+use pernixc_source_file::SourceElement;
 
+use crate::{
+    bind::{Bind, Config, Expression},
+    binder::{BindingError, Error},
+    diagnostic::Diagnostic,
+};
+
+impl Bind<&pernixc_syntax::expression::postfix::Postfix>
+    for crate::binder::Binder<'_>
+{
+    async fn bind(
+        &mut self,
+        syntax_tree: &pernixc_syntax::expression::postfix::Postfix,
+        config: &Config<'_>,
+        handler: &dyn Handler<Diagnostic>,
+    ) -> Result<Expression, Error> {
+        let Some(unit) = syntax_tree.unit() else {
+            return Err(Error::Binding(BindingError(syntax_tree.span())));
+        };
+
+        let postfix_operators = syntax_tree.operators().collect::<Vec<_>>();
+
+        // directly bind the unit if there are no postfix operators
+        if postfix_operators.is_empty() {
+            return self.bind(&unit, config, handler).await;
+        }
+
+        // otherwise, bind as an rvalue first
+        for operator in postfix_operators {
+            match operator {
+                pernixc_syntax::expression::postfix::Operator::MethodCall(
+                    _method_call,
+                ) => todo!(),
+                pernixc_syntax::expression::postfix::Operator::Cast(_cast) => {
+                    todo!()
+                }
+                pernixc_syntax::expression::postfix::Operator::Access(
+                    _access,
+                ) => todo!(),
+            }
+        }
+
+        todo!()
+    }
+}
