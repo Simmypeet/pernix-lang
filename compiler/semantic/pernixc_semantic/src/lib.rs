88cb6b13fbb6bc822885e6955ad99fbe06f3a4ec
Simmypeet <simmypeet@outlook.com>
2025-09-16 23:05:27 +0700

    remove unused files

--- compiler/semantic/pernixc_semantic/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Contains the definition of [`table::Table`], the final data structure that
-//! will be used to codegen.
-
-pub mod component;
-pub mod diagnostic;
-pub mod table;
-pub mod term;
