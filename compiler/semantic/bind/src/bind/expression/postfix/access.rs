2e9de92e6d0e4cf188a725343025fc6bae89536c
Simmypeet <simmypeet@outlook.com>
2025-09-17 01:20:27 +0700

    refactor access file

--- compiler/semantic/bind/src/bind/expression/postfix/access.rs
+++ compiler/semantic/bind/src/bind/expression/postfix/access.rs
@@ -115,6 +115,8 @@ pub(super) async fn bind_access(
 
     let reduced_lvalue =
         reduce_address_reference(binder, lvalue, current_span, handler).await?;
+    let qualifier = reduced_lvalue.qualifier;
+
     let address_ty =
         binder.type_of_address(&reduced_lvalue.address, handler).await?;
 
@@ -125,207 +127,31 @@ pub(super) async fn bind_access(
 
     let address = match kind {
         AccessKind::StructField(ident) => {
-            let struct_id = match &address_ty {
-                Type::Symbol(Symbol { id: struct_id, .. })
-                    if {
-                        let symbol_kind =
-                            binder.engine().get_kind(*struct_id).await;
-
-                        symbol_kind == Kind::Struct
-                    } =>
-                {
-                    *struct_id
-                }
-
-                _ => {
-                    handler.receive(
-                        Diagnostic::UnexpectedTypeForAccess(
-                            UnexpectedTypeForAccess {
-                                expected_type: diagnostic::ExpectedType::Struct,
-                                span: current_span,
-                                found_type: address_ty,
-                                type_inference_map: binder
-                                    .type_inference_rendering_map(),
-                                constant_inference_map: binder
-                                    .constant_inference_rendering_map(),
-                            },
-                        )
-                        .into(),
-                    );
-
-                    return Err(Error::Binding(BindingError(new_span)));
-                }
-            };
-
-            let fields = binder.engine().get_fields(struct_id).await?;
-
-            let Some(field_id) =
-                fields.field_ids_by_name.get(ident.kind.as_str()).copied()
-            else {
-                handler.receive(
-                    Diagnostic::FieldNotFound(FieldNotFound {
-                        identifier_span: ident.span(),
-                        struct_id,
-                        field_name: ident.kind.0,
-                    })
-                    .into(),
-                );
-
-                return Err(Error::Binding(BindingError(new_span)));
-            };
-
-            let field_accessibility = fields.fields[field_id].accessibility;
-
-            // field is not accessible, soft error, keep going
-            if !binder
-                .engine()
-                .is_accessible_from_globally(
-                    binder.current_site(),
-                    field_accessibility.into_global(struct_id.target_id),
-                )
-                .await
-            {
-                handler.receive(
-                    Diagnostic::FieldIsNotAccessible(FieldIsNotAccessible {
-                        field_id,
-                        struct_id,
-                        referring_site: binder.current_site(),
-                        referring_identifier_span: ident.span(),
-                    })
-                    .into(),
-                );
-            }
-
-            Address::Field(address::Field {
-                struct_address: Box::new(reduced_lvalue.address),
-                id: field_id,
-            })
+            access_struct(
+                binder,
+                reduced_lvalue,
+                ident,
+                current_span,
+                new_span,
+                address_ty,
+                handler,
+            )
+            .await?
         }
 
-        AccessKind::TupleIndex(tuple_index) => {
-            let tuple_ty = match address_ty {
-                Type::Tuple(tuple_ty) => tuple_ty,
-                found_type => {
-                    handler.receive(
-                        Diagnostic::UnexpectedTypeForAccess(
-                            UnexpectedTypeForAccess {
-                                expected_type: diagnostic::ExpectedType::Tuple,
-                                span: current_span,
-                                found_type,
-                                type_inference_map: binder
-                                    .type_inference_rendering_map(),
-                                constant_inference_map: binder
-                                    .constant_inference_rendering_map(),
-                            },
-                        )
-                        .into(),
-                    );
+        AccessKind::TupleIndex(tuple_index) => access_tuple(
+            binder,
+            reduced_lvalue,
+            &tuple_index,
+            current_span,
+            new_span,
+            address_ty,
+            handler,
+        )?,
 
-                    return Err(Error::Binding(BindingError(new_span)));
-                }
-            };
-
-            // sanity check
-            if tuple_ty.elements.iter().filter(|x| x.is_unpacked).count() > 1 {
-                return Err(Error::Binding(BindingError(new_span)));
-            }
-
-            // used to chec if the index is past the unpacked index
-            let unpacked_index =
-                tuple_ty.elements.iter().position(|x| x.is_unpacked);
-
-            let Some(index_str) = tuple_index.index() else {
-                return Err(Error::Binding(BindingError(new_span)));
-            };
-
-            let index = match index_str.kind.0.parse::<usize>() {
-                Ok(number) => number,
-                Err(err) => match err.kind() {
-                    std::num::IntErrorKind::NegOverflow
-                    | std::num::IntErrorKind::PosOverflow => {
-                        handler.receive(
-                            Diagnostic::TooLargeTupleIndex(
-                                TooLargeTupleIndex {
-                                    access_span: access.span(),
-                                },
-                            )
-                            .into(),
-                        );
-
-                        return Err(Error::Binding(BindingError(current_span)));
-                    }
-
-                    _ => {
-                        unreachable!()
-                    }
-                },
-            };
-
-            if index >= tuple_ty.elements.len() {
-                handler.receive(
-                    Diagnostic::TupleIndexOutOfBounds(TupleIndexOutOfBounds {
-                        access_span: access.span(),
-                        element_count: tuple_ty.elements.len(),
-                        accessed_index: index,
-                    })
-                    .into(),
-                );
-
-                return Err(Error::Binding(BindingError(current_span)));
-            }
-
-            if let Some(unpacked_index) = unpacked_index {
-                // can't access past the unpacked index
-                let pass_unpacked = if tuple_index.minus().is_some() {
-                    index.cmp(&(tuple_ty.elements.len() - unpacked_index - 1))
-                } else {
-                    index.cmp(&unpacked_index)
-                };
-
-                // report error
-                if pass_unpacked.is_ge() {
-                    handler.receive(
-                        Diagnostic::CannotIndexPastUnpackedTuple(
-                            CannotIndexPastUnpackedTuple {
-                                index_span: tuple_index.span(),
-                                tuple_type: tuple_ty,
-                                unpacked_position: unpacked_index,
-                                offset: match (
-                                    pass_unpacked.is_eq(),
-                                    tuple_index.minus(),
-                                ) {
-                                    (true, _) => address::Offset::Unpacked,
-                                    (_, Some(_)) => {
-                                        address::Offset::FromEnd(index)
-                                    }
-                                    (_, None) => {
-                                        address::Offset::FromStart(index)
-                                    }
-                                },
-                                type_inference_map: binder
-                                    .type_inference_rendering_map(),
-                                constant_inference_map: binder
-                                    .constant_inference_rendering_map(),
-                            },
-                        )
-                        .into(),
-                    );
-
-                    return Err(Error::Binding(BindingError(current_span)));
-                }
-            }
-
-            Address::Tuple(address::Tuple {
-                tuple_address: Box::new(reduced_lvalue.address),
-                offset: if tuple_index.minus().is_some() {
-                    address::Offset::FromEnd(index)
-                } else {
-                    address::Offset::FromStart(index)
-                },
-            })
+        AccessKind::ArrayIndex(_array_index) => {
+            todo!()
         }
-
-        AccessKind::ArrayIndex(_array_index) => todo!(),
     };
 
     let joined_span = new_span;
@@ -346,9 +172,212 @@ pub(super) async fn bind_access(
             Expression::LValue(LValue {
                 address,
                 span: joined_span,
-                qualifier: reduced_lvalue.qualifier,
+                qualifier,
             }),
             joined_span,
         )),
     }
+}
+
+async fn access_struct(
+    binder: &mut crate::binder::Binder<'_>,
+    reduced_lvalue: LValue,
+    ident: pernixc_syntax::Identifier,
+    current_span: RelativeSpan,
+    new_span: RelativeSpan,
+    address_ty: Type,
+    handler: &dyn pernixc_handler::Handler<crate::diagnostic::Diagnostic>,
+) -> Result<Address, Error> {
+    let struct_id = match &address_ty {
+        Type::Symbol(Symbol { id: struct_id, .. })
+            if {
+                let symbol_kind = binder.engine().get_kind(*struct_id).await;
+
+                symbol_kind == Kind::Struct
+            } =>
+        {
+            *struct_id
+        }
+
+        _ => {
+            handler.receive(
+                Diagnostic::UnexpectedTypeForAccess(UnexpectedTypeForAccess {
+                    expected_type: diagnostic::ExpectedType::Struct,
+                    span: current_span,
+                    found_type: address_ty,
+                    type_inference_map: binder.type_inference_rendering_map(),
+                    constant_inference_map: binder
+                        .constant_inference_rendering_map(),
+                })
+                .into(),
+            );
+
+            return Err(Error::Binding(BindingError(new_span)));
+        }
+    };
+
+    let fields = binder.engine().get_fields(struct_id).await?;
+
+    let Some(field_id) =
+        fields.field_ids_by_name.get(ident.kind.as_str()).copied()
+    else {
+        handler.receive(
+            Diagnostic::FieldNotFound(FieldNotFound {
+                identifier_span: ident.span(),
+                struct_id,
+                field_name: ident.kind.0,
+            })
+            .into(),
+        );
+
+        return Err(Error::Binding(BindingError(new_span)));
+    };
+
+    let field_accessibility = fields.fields[field_id].accessibility;
+
+    // field is not accessible, soft error, keep going
+    if !binder
+        .engine()
+        .is_accessible_from_globally(
+            binder.current_site(),
+            field_accessibility.into_global(struct_id.target_id),
+        )
+        .await
+    {
+        handler.receive(
+            Diagnostic::FieldIsNotAccessible(FieldIsNotAccessible {
+                field_id,
+                struct_id,
+                referring_site: binder.current_site(),
+                referring_identifier_span: ident.span(),
+            })
+            .into(),
+        );
+    }
+
+    Ok(Address::Field(address::Field {
+        struct_address: Box::new(reduced_lvalue.address),
+        id: field_id,
+    }))
+}
+
+fn access_tuple(
+    binder: &mut crate::binder::Binder<'_>,
+    reduced_lvalue: LValue,
+    tuple_index: &pernixc_syntax::expression::postfix::TupleIndex,
+    current_span: RelativeSpan,
+    new_span: RelativeSpan,
+    address_ty: Type,
+    handler: &dyn pernixc_handler::Handler<crate::diagnostic::Diagnostic>,
+) -> Result<Address, Error> {
+    let tuple_ty = match address_ty {
+        Type::Tuple(tuple_ty) => tuple_ty,
+        found_type => {
+            handler.receive(
+                Diagnostic::UnexpectedTypeForAccess(UnexpectedTypeForAccess {
+                    expected_type: diagnostic::ExpectedType::Tuple,
+                    span: current_span,
+                    found_type,
+                    type_inference_map: binder.type_inference_rendering_map(),
+                    constant_inference_map: binder
+                        .constant_inference_rendering_map(),
+                })
+                .into(),
+            );
+
+            return Err(Error::Binding(BindingError(new_span)));
+        }
+    };
+
+    // sanity check
+    if tuple_ty.elements.iter().filter(|x| x.is_unpacked).count() > 1 {
+        return Err(Error::Binding(BindingError(new_span)));
+    }
+
+    // used to chec if the index is past the unpacked index
+    let unpacked_index = tuple_ty.elements.iter().position(|x| x.is_unpacked);
+
+    let Some(index_str) = tuple_index.index() else {
+        return Err(Error::Binding(BindingError(new_span)));
+    };
+
+    let index = match index_str.kind.0.parse::<usize>() {
+        Ok(number) => number,
+        Err(err) => match err.kind() {
+            std::num::IntErrorKind::NegOverflow
+            | std::num::IntErrorKind::PosOverflow => {
+                handler.receive(
+                    Diagnostic::TooLargeTupleIndex(TooLargeTupleIndex {
+                        access_span: index_str.span,
+                    })
+                    .into(),
+                );
+
+                return Err(Error::Binding(BindingError(current_span)));
+            }
+
+            _ => {
+                unreachable!()
+            }
+        },
+    };
+
+    if index >= tuple_ty.elements.len() {
+        handler.receive(
+            Diagnostic::TupleIndexOutOfBounds(TupleIndexOutOfBounds {
+                access_span: index_str.span,
+                element_count: tuple_ty.elements.len(),
+                accessed_index: index,
+            })
+            .into(),
+        );
+
+        return Err(Error::Binding(BindingError(current_span)));
+    }
+
+    if let Some(unpacked_index) = unpacked_index {
+        // can't access past the unpacked index
+        let pass_unpacked = if tuple_index.minus().is_some() {
+            index.cmp(&(tuple_ty.elements.len() - unpacked_index - 1))
+        } else {
+            index.cmp(&unpacked_index)
+        };
+
+        // report error
+        if pass_unpacked.is_ge() {
+            handler.receive(
+                Diagnostic::CannotIndexPastUnpackedTuple(
+                    CannotIndexPastUnpackedTuple {
+                        index_span: tuple_index.span(),
+                        tuple_type: tuple_ty,
+                        unpacked_position: unpacked_index,
+                        offset: match (
+                            pass_unpacked.is_eq(),
+                            tuple_index.minus(),
+                        ) {
+                            (true, _) => address::Offset::Unpacked,
+                            (_, Some(_)) => address::Offset::FromEnd(index),
+                            (_, None) => address::Offset::FromStart(index),
+                        },
+                        type_inference_map: binder
+                            .type_inference_rendering_map(),
+                        constant_inference_map: binder
+                            .constant_inference_rendering_map(),
+                    },
+                )
+                .into(),
+            );
+
+            return Err(Error::Binding(BindingError(current_span)));
+        }
+    }
+
+    Ok(Address::Tuple(address::Tuple {
+        tuple_address: Box::new(reduced_lvalue.address),
+        offset: if tuple_index.minus().is_some() {
+            address::Offset::FromEnd(index)
+        } else {
+            address::Offset::FromStart(index)
+        },
+    }))
 }
