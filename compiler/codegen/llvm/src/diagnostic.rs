68eaafb1c50757136165707789b82bc0831dc697
Simmypeet <simmypeet@outlook.com>
2025-09-19 02:14:32 +0700

    fix diagnostic file

--- compiler/codegen/llvm/src/diagnostic.rs
+++ compiler/codegen/llvm/src/diagnostic.rs
@@ -1,13 +1,13 @@
 //! Contains the diagnostics used in the code generation process.
 
-use pernixc_diagnostic::{Diagnostic, Report};
-use pernixc_log::Severity;
-use pernixc_semantic::{
-    component::input::LocationSpan,
-    table::{GlobalID, Table},
-};
+use pernixc_diagnostic::{ByteIndex, Highlight, Rendered, Report};
+use pernixc_query::{runtime::executor::CyclicError, TrackedEngine};
+use pernixc_symbol::{source_map::to_absolute_span, span::get_span};
+use pernixc_target::Global;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+/// An enumeration of diagnostics that can occur during code generation.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[allow(missing_docs)]
 pub enum Diagnostic {
     MainIsNotAFunction(MainIsNotAFunction),
     InvalidMainFunctionSignature(InvalidMainFunctionSignature),
@@ -19,28 +19,52 @@ pub enum Diagnostic {
     ),
 }
 
+impl Report for Diagnostic {
+    async fn report(
+        &self,
+        engine: &TrackedEngine,
+    ) -> Result<Rendered<ByteIndex>, CyclicError> {
+        match self {
+            Self::MainIsNotAFunction(diag) => diag.report(engine).await,
+            Self::InvalidMainFunctionSignature(diag) => {
+                diag.report(engine).await
+            }
+            Self::GenericParametersAreNotAllowedInMainFunction(diag) => {
+                diag.report(engine).await
+            }
+            Self::WhereClausePredicatesAreNotAllowedInMainFunction(
+                diag,
+            ) => diag.report(engine).await,
+        }
+    }
+}
+
 /// The `main` symbol is reserved from the main function.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct MainIsNotAFunction {
     /// The ID of the main function.
-    pub main_function_id: GlobalID,
+    pub main_function_id: Global<pernixc_symbol::ID>,
 }
 
-impl Report<&Table> for MainIsNotAFunction {
-    fn report(&self, table: &Table) -> Diagnostic {
-        let span = table
-            .get::<LocationSpan>(self.main_function_id)
-            .span
-            .clone()
-            .unwrap();
+impl Report for MainIsNotAFunction {
+    async fn report(
+        &self,
+        parameter: &TrackedEngine,
+    ) -> Result<Rendered<ByteIndex>, CyclicError> {
+        let span = parameter
+            .to_absolute_span(
+                &parameter.get_span(self.main_function_id).await.unwrap(),
+            )
+            .await;
 
-        Diagnostic {
-            span,
-            message: "`main` is reserved for functions only".to_string(),
-            severity: Severity::Error,
-            help_message: None,
-            related: Vec::new(),
-        }
+        Ok(Rendered::builder()
+            .message(
+                "the `main` symbol is reserved for the main function and must \
+                 be a function with the signature `function() -> int32`",
+            )
+            .primary_highlight(Highlight::builder().span(span).build())
+            .help_message("consider renaming this symbol to something else")
+            .build())
     }
 }
 
@@ -48,25 +72,30 @@ impl Report<&Table> for MainIsNotAFunction {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InvalidMainFunctionSignature {
     /// The ID of the main function.
-    pub main_function_id: GlobalID,
+    pub main_function_id: Global<pernixc_symbol::ID>,
 }
 
-impl Report<&Table> for InvalidMainFunctionSignature {
-    fn report(&self, table: &Table) -> Diagnostic {
-        let span = table
-            .get::<LocationSpan>(self.main_function_id)
-            .span
-            .clone()
-            .unwrap();
+impl Report for InvalidMainFunctionSignature {
+    async fn report(
+        &self,
+        parameter: &TrackedEngine,
+    ) -> Result<Rendered<ByteIndex>, CyclicError> {
+        let span = parameter
+            .to_absolute_span(
+                &parameter.get_span(self.main_function_id).await.unwrap(),
+            )
+            .await;
 
-        Diagnostic {
-            span,
-            message: "main function must have the signature `function()`"
-                .to_string(),
-            severity: Severity::Error,
-            help_message: None,
-            related: Vec::new(),
-        }
+        Ok(Rendered::builder()
+            .message(
+                "the `main` function must have the signature `function() -> \
+                 int32`",
+            )
+            .primary_highlight(Highlight::builder().span(span).build())
+            .help_message(
+                "consider changing the signature of the main function",
+            )
+            .build())
     }
 }
 
@@ -74,25 +103,27 @@ impl Report<&Table> for InvalidMainFunctionSignature {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct GenericParametersAreNotAllowedInMainFunction {
     /// The ID of the main function
-    pub main_function_id: GlobalID,
+    pub main_function_id: Global<pernixc_symbol::ID>,
 }
 
-impl Report<&Table> for GenericParametersAreNotAllowedInMainFunction {
-    fn report(&self, table: &Table) -> Diagnostic {
-        let span = table
-            .get::<LocationSpan>(self.main_function_id)
-            .span
-            .clone()
-            .unwrap();
+impl Report for GenericParametersAreNotAllowedInMainFunction {
+    async fn report(
+        &self,
+        parameter: &TrackedEngine,
+    ) -> Result<Rendered<ByteIndex>, CyclicError> {
+        let span = parameter
+            .to_absolute_span(
+                &parameter.get_span(self.main_function_id).await.unwrap(),
+            )
+            .await;
 
-        Diagnostic {
-            span,
-            message: "generic parameters are not allowed in main function"
-                .to_string(),
-            severity: Severity::Error,
-            help_message: None,
-            related: Vec::new(),
-        }
+        Ok(Rendered::builder()
+            .message(
+                "the `main` function is not allowed to have generic parameters",
+            )
+            .primary_highlight(Highlight::builder().span(span).build())
+            .help_message("consider removing the generic parameters")
+            .build())
     }
 }
 
@@ -100,24 +131,27 @@ impl Report<&Table> for GenericParametersAreNotAllowedInMainFunction {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct WhereClausePredicatesAreNotAllowedInMainFunction {
     /// The ID of the main function
-    pub main_function_id: GlobalID,
+    pub main_function_id: Global<pernixc_symbol::ID>,
 }
 
-impl Report<&Table> for WhereClausePredicatesAreNotAllowedInMainFunction {
-    fn report(&self, table: &Table) -> Diagnostic {
-        let span = table
-            .get::<LocationSpan>(self.main_function_id)
-            .span
-            .clone()
-            .unwrap();
+impl Report for WhereClausePredicatesAreNotAllowedInMainFunction {
+    async fn report(
+        &self,
+        parameter: &TrackedEngine,
+    ) -> Result<Rendered<ByteIndex>, CyclicError> {
+        let span = parameter
+            .to_absolute_span(
+                &parameter.get_span(self.main_function_id).await.unwrap(),
+            )
+            .await;
 
-        Diagnostic {
-            span,
-            message: "where clause predicates are not allowed in main function"
-                .to_string(),
-            severity: Severity::Error,
-            help_message: None,
-            related: Vec::new(),
-        }
+        Ok(Rendered::builder()
+            .message(
+                "the `main` function is not allowed to have where clause \
+                 predicates",
+            )
+            .primary_highlight(Highlight::builder().span(span).build())
+            .help_message("consider removing the where clause predicates")
+            .build())
     }
 }
