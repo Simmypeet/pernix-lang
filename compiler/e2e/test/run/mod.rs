ecf3aeea195b2aab864603414385b0c57160f2e7
Simmypeet <simmypeet@outlook.com>
2025-09-20 18:11:13 +0700

    replace \r\n to \n

--- compiler/e2e/test/run/mod.rs
+++ compiler/e2e/test/run/mod.rs
@@ -170,16 +170,21 @@ async fn test(file_path: &Path) {
         // Get output
         let output = child.wait_with_output().unwrap();
 
-        let stdout = String::from_utf8(output.stdout).unwrap();
+        // replace \r\n with \n
+        let stdout = String::from_utf8(output.stdout)
+            .unwrap()
+            .trim()
+            .replace("\r\n", "\n");
+        let stderr = String::from_utf8(output.stderr)
+            .unwrap()
+            .trim()
+            .replace("\r\n", "\n");
 
         run_results.push(RunResult {
             exit_code: output.status.code(),
             stdin: stdin_content.trim().to_string(),
-            stdout: stdout.trim().to_string(),
-            stderr: String::from_utf8(output.stderr)
-                .unwrap()
-                .trim()
-                .to_string(),
+            stdout,
+            stderr,
         });
     }
 
