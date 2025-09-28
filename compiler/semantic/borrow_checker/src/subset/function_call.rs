8239765e29aa53e694221000b1a303f6ded28bed
Simmypeet <simmypeet@outlook.com>
2025-09-28 21:03:18 +0700

    add borrow checker check for effect operation

--- compiler/semantic/borrow_checker/src/subset/function_call.rs
+++ compiler/semantic/borrow_checker/src/subset/function_call.rs
@@ -3,21 +3,98 @@ use std::collections::BTreeSet;
 use pernixc_hash::HashSet;
 use pernixc_ir::value::register::FunctionCall;
 use pernixc_lexical::tree::RelativeSpan;
-use pernixc_semantic_element::{parameter::get_parameters, variance::Variance};
+use pernixc_semantic_element::{
+    do_effect::get_do_effects, parameter::get_parameters, variance::Variance,
+};
 use pernixc_symbol::{
     kind::{get_kind, Kind},
-    parent::get_parent,
+    parent::{get_parent, get_parent_global},
 };
 use pernixc_target::Global;
 use pernixc_term::{
+    generic_arguments::Symbol,
     generic_parameters::get_generic_parameters,
     predicate::{PositiveTrait, Predicate},
 };
-use pernixc_type_system::{normalizer::Normalizer, UnrecoverableError};
+use pernixc_type_system::{
+    lifetime_constraint::LifetimeConstraint, normalizer::Normalizer,
+    UnrecoverableError,
+};
 
 use crate::{context::Context, subset::Changes, Region};
 
 impl<N: Normalizer> Context<'_, N> {
+    #[allow(clippy::too_many_lines)]
+    pub(super) async fn get_changes_of_effect_operation(
+        &self,
+        function_call: &FunctionCall,
+        span: &RelativeSpan,
+        lifetime_constraints: &mut BTreeSet<LifetimeConstraint>,
+    ) -> Result<(), UnrecoverableError> {
+        let parent_effect_id = self
+            .tracked_engine()
+            .get_parent_global(function_call.callable_id)
+            .await
+            .unwrap();
+
+        let parent_generic_parameters = self
+            .tracked_engine()
+            .get_generic_parameters(parent_effect_id)
+            .await?;
+
+        let effect_operation = Symbol {
+            id: parent_effect_id,
+            generic_arguments: function_call
+                .instantiation
+                .create_generic_arguments(
+                    parent_effect_id,
+                    &parent_generic_parameters,
+                ),
+        };
+
+        let do_effects =
+            self.tracked_engine().get_do_effects(self.current_site()).await?;
+
+        let mut found = false;
+        for available_cap in &do_effects.effects {
+            // must be the same effect
+            if available_cap.id != effect_operation.id {
+                continue;
+            }
+
+            let Some(subtypable) = self
+                .environment()
+                .subtypes_generic_arguments(
+                    &effect_operation.generic_arguments,
+                    &available_cap.generic_arguments,
+                )
+                .await
+                .map_err(|e| {
+                    e.report_as_type_calculating_overflow(
+                        *span,
+                        &self.handler(),
+                    )
+                })?
+            else {
+                continue;
+            };
+
+            assert!(subtypable.result.forall_lifetime_errors.is_empty());
+
+            lifetime_constraints.extend(subtypable.constraints.iter().cloned());
+
+            found = true;
+            break;
+        }
+
+        assert!(
+            found,
+            "in borrow checking, all effect operations should be valid"
+        );
+
+        Ok(())
+    }
+
     #[allow(clippy::too_many_lines)]
     pub(super) async fn get_changes_of_function_call(
         &self,
@@ -138,26 +215,6 @@ impl<N: Normalizer> Context<'_, N> {
             }
 
             Kind::EffectOperation => {
-                let parent_effect_id = Global::new(
-                    function_call.callable_id.target_id,
-                    self.tracked_engine()
-                        .get_parent(function_call.callable_id)
-                        .await
-                        .unwrap(),
-                );
-
-                lifetime_constraints.extend(
-                    self.environment()
-                        .wf_check(
-                            parent_effect_id,
-                            *span,
-                            &function_call.instantiation,
-                            false,
-                            &self.handler(),
-                        )
-                        .await?,
-                );
-
                 lifetime_constraints.extend(
                     self.environment()
                         .wf_check(
@@ -169,6 +226,13 @@ impl<N: Normalizer> Context<'_, N> {
                         )
                         .await?,
                 );
+
+                self.get_changes_of_effect_operation(
+                    function_call,
+                    span,
+                    &mut lifetime_constraints,
+                )
+                .await?;
             }
 
             _ => unreachable!(
