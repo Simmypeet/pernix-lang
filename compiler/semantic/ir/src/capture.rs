494580ca4a17203160a5505325b7914847da8de4
Simmypeet <simmypeet@outlook.com>
2025-11-02 07:32:49 +0700

    remove mutable indexing

--- compiler/semantic/ir/src/capture.rs
+++ compiler/semantic/ir/src/capture.rs
@@ -1,7 +1,7 @@
 //! Defines the [`Captures`] representing the capturing structure used for
 //! implementing closures, do blocks, and effect handlers.
 
-use derive_more::{Index, IndexMut};
+use derive_more::Index;
 use flexstr::SharedStr;
 use pernixc_arena::Arena;
 use pernixc_lexical::tree::RelativeSpan;
@@ -31,29 +31,14 @@ use crate::{
     Deserialize,
     Default,
     Index,
-    IndexMut,
 )]
 pub struct Captures {
     /// All the captures used in the closure.
     #[index]
-    #[index_mut]
     captures: Arena<Capture>,
 }
 
 impl Captures {
-    /// Inserts a new capture into the capturing structure.
-    pub fn insert(&mut self, capture: Capture) -> pernixc_arena::ID<Capture> {
-        self.captures.insert(capture)
-    }
-
-    /// Removes a capture from the capturing structure.
-    pub fn remove(
-        &mut self,
-        capture_id: pernixc_arena::ID<Capture>,
-    ) -> Option<Capture> {
-        self.captures.remove(capture_id)
-    }
-
     /// Returns an iterator over all capture IDs in the capturing structure.
     #[must_use]
     pub fn ids(
