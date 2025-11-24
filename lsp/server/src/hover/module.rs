e95b0284a0f49f872fc2d62e45302c0dd2c56d7e
Simmypeet <simmypeet@outlook.com>
2025-11-24 15:04:28 +0700

    fix incorrect fence language

--- lsp/server/src/hover/module.rs
+++ lsp/server/src/hover/module.rs
@@ -5,18 +5,20 @@ use pernixc_query::TrackedEngine;
 use pernixc_symbol::name::get_qualified_name;
 use pernixc_target::Global;
 
-use crate::formatter::accessbility::get_accessiblity_str;
+use crate::{
+    formatter::accessbility::get_accessiblity_str,
+    hover::markdown::PERNIX_FENCE,
+};
 
 #[extend]
 pub async fn format_module_signature(
     self: &TrackedEngine,
     symbol: Global<pernixc_symbol::ID>,
 ) -> String {
-    let mut markdown = String::new();
+    let mut markdown = format!("```{PERNIX_FENCE}\n");
     let qualified_name = self.get_qualified_name(symbol).await;
     let accessiblity_str = self.get_accessiblity_str(symbol).await;
 
-    markdown.push_str("```pernixc\n");
     write!(markdown, "{accessiblity_str} module {qualified_name}").unwrap();
 
     markdown.push_str("\n```\n");
