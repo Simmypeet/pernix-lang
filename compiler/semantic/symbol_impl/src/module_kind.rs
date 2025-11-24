e761de2e158800acab3f4174b8287eab9b9b3e04
Simmypeet <simmypeet@outlook.com>
2025-11-24 15:45:08 +0700

    fix bugs in module_kind

--- compiler/semantic/symbol_impl/src/module_kind.rs
+++ compiler/semantic/symbol_impl/src/module_kind.rs
@@ -1,6 +1,10 @@
 use pernixc_query::{TrackedEngine, runtime::executor::CyclicError};
 use pernixc_source_file::calculate_path_id;
-use pernixc_symbol::module_kind::{Key, ModuleKind};
+use pernixc_symbol::{
+    module_kind::{Key, ModuleKind},
+    parent::get_parent_global,
+};
+use pernixc_target::get_invocation_arguments;
 
 use crate::table::get_table_of_symbol;
 
@@ -9,7 +13,20 @@ pub async fn module_kind_executor(
     &Key(key): &Key,
     engine: &TrackedEngine,
 ) -> Result<ModuleKind, CyclicError> {
-    let map = engine.get_table_of_symbol(key).await;
+    // if no parent module, the module is at the root.
+    let Some(parent_module_id) = engine.get_parent_global(key).await else {
+        let target_args = engine.get_invocation_arguments(key.target_id).await;
+        let root_src_id = engine
+            .calculate_path_id(&target_args.command.input().file, key.target_id)
+            .await
+            .ok();
+
+        return Ok(ModuleKind::ExteranlFile(root_src_id));
+    };
+
+    // gets the map of the parent, which should have an external_submodules
+    // containing the current module id, if it's an external module
+    let map = engine.get_table_of_symbol(parent_module_id).await;
 
     // if the module presents in the map.external_submodules, it is an external
     // module.
