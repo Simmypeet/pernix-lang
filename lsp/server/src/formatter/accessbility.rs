c8643d645e552e24856e182fc112e70f21617ae8
Simmypeet <simmypeet@outlook.com>
2025-11-24 14:59:47 +0700

    add module signature format

--- lsp/server/src/formatter/accessbility.rs
+++ lsp/server/src/formatter/accessbility.rs
@@ -3,21 +3,26 @@
 use pernixc_extend::extend;
 use pernixc_query::TrackedEngine;
 use pernixc_symbol::{
-    accessibility::get_accessibility, get_target_root_module_id,
-    parent::get_closest_module_id,
+    accessibility::get_accessibility,
+    get_target_root_module_id,
+    parent::{get_closest_module_id, get_parent_global},
 };
 use pernixc_target::Global;
 
 /// Gets the accessibility string for the given symbol.
 #[extend]
-pub(super) async fn get_accessiblity_str(
+pub async fn get_accessiblity_str(
     self: &TrackedEngine,
     symbol_id: Global<pernixc_symbol::ID>,
 ) -> String {
     let accessibility = self.get_accessibility(symbol_id).await;
     let root_module_id =
         self.get_target_root_module_id(symbol_id.target_id).await;
-    let nearest_moodule_id = self.get_closest_module_id(symbol_id).await;
+
+    let parent_id =
+        self.get_parent_global(symbol_id).await.unwrap_or(symbol_id);
+
+    let nearest_moodule_id = self.get_closest_module_id(parent_id).await;
 
     let accessibility_str = match accessibility {
         pernixc_symbol::accessibility::Accessibility::Public => "public",
