1e6fdc3fc657fa37b5098f7adc17e716ea56c0a7
Simmypeet <simmypeet@outlook.com>
2025-12-04 18:50:36 +0700

    enable lsp trace for vscode

--- lsp/client/code/src/extension.ts
+++ lsp/client/code/src/extension.ts
@@ -1,21 +1,28 @@
-import os = require('os');
-import path = require('path');
-import { workspace, window, ExtensionContext } from 'vscode';
+import os = require("os");
+import path = require("path");
+import { workspace, window, ExtensionContext } from "vscode";
 
 import {
     Executable,
     LanguageClient,
     LanguageClientOptions,
     ServerOptions,
-} from 'vscode-languageclient/node';
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
     // The server should be in ~/.pernix/bin/pernix_server
-    let serverModule = path.join(os.homedir(), '.pernix', 'bin', 'pernix_server');
+    let serverModule = path.join(
+        os.homedir(),
+        ".pernix",
+        "bin",
+        "pernix_server"
+    );
 
-    const traceOutputChannel = window.createOutputChannel("Pernix Language Server Trace");
+    const traceOutputChannel = window.createOutputChannel(
+        "Pernix Language Server Trace"
+    );
     const run: Executable = {
         command: serverModule,
         options: { env: { ...process.env, RUST_LOG: "debug" } },
@@ -23,23 +30,22 @@ export function activate(context: ExtensionContext) {
 
     let serverOptions: ServerOptions = {
         run,
-        debug: run
+        debug: run,
     };
 
     // Options to control the language client
     let clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'pernix' }],
+        documentSelector: [{ scheme: "file", language: "pernix" }],
         synchronize: {
-            fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+            fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
         },
-        traceOutputChannel
+        traceOutputChannel,
     };
-
 
     // Create the language client and start the client.
     client = new LanguageClient(
-        'pernix-language-server',
-        'Pernix Language Server',
+        "pernix",
+        "Pernix Language Server",
         serverOptions,
         clientOptions
     );
