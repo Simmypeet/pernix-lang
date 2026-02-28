8cbc0a8834cd134e8a77130bcf4b03decc2fdb9f
Simmypeet <simmypeet@outlook.com>
2026-02-28 20:13:49 +0700

    feat: add global_instance query

--- compiler/semantic/semantic_element/src/global_instances.rs
+++ compiler/semantic/semantic_element/src/global_instances.rs
@@ -1,15 +1,16 @@
-//! Defines a query for retrieving all the `implements` IDs that are associated
-//! with a given symbol.
+//! Defines a query for retrieving all the `instances` IDs that are associated
+//! with a `trait` symbol.
+//!
+//! This will only include the `instance` IDs that "ARE NOT EXTERNAL".
 
 use pernixc_hash::HashSet;
-use pernixc_target::{Global, TargetID};
+use pernixc_target::Global;
 use qbice::{Decode, Encode, Query, StableHash, storage::intern::Interned};
 
-/// A query for retrieving all the `implements`/`instance` IDs that implements
-/// this symbol.
+/// A query for retrieving all the `instance` IDs that are associated with a
+/// `trait` symbol.
 ///
-/// This will include all the `implements` IDs that implemented the `symbol_id`
-/// in all the downstream targets.
+/// This will only include the `instance` IDs that "ARE NOT EXTERNAL".
 #[derive(
     Debug,
     Clone,
@@ -25,16 +26,13 @@ use qbice::{Decode, Encode, Query, StableHash, storage::intern::Interned};
     Query,
 )]
 #[value(Interned<HashSet<Global<pernixc_symbol::ID>>>)]
-#[extend(name = get_implemented, by_val)]
+#[extend(name = get_global_instances_of, by_val)]
 pub struct Key {
     /// The global ID of the symbol to get implementations for.
     pub symbol_id: Global<pernixc_symbol::ID>,
 
-    /// The target ID where the query was made.
-    ///
-    /// This is used to scan for all downstream targets to find all the
-    /// implementations of this symbol.
-    pub target_id: TargetID,
+    /// The target ID to search for implementations in.
+    pub target_id: pernixc_target::TargetID,
 }
 
 /// A query for retrieving all the `implements` IDs that implements this symbol
