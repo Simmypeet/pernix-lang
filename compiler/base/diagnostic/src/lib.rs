27a9fee2e011a720d0bc3751ad03c71784ae036f
Simmypeet <simmypeet@outlook.com>
2026-03-01 21:25:33 +0700

    refactor: refactor Rendered builder

--- compiler/base/diagnostic/src/lib.rs
+++ compiler/base/diagnostic/src/lib.rs
@@ -80,6 +80,16 @@ pub struct Highlight<L> {
     message: Option<String>,
 }
 
+impl<L> Highlight<L> {
+    /// Retrieves the span of the highlight.
+    #[must_use]
+    pub const fn span(&self) -> &Span<L> { &self.span }
+
+    /// Retrieves the message of the highlight, if any.
+    #[must_use]
+    pub fn message(&self) -> Option<&str> { self.message.as_deref() }
+}
+
 /// A strucut representing a diagnostic message ready to be displayed to the
 /// user.
 #[derive(
@@ -99,21 +109,9 @@ pub struct Rendered<L> {
     /// The severity of the diagnostic.
     severity: Severity,
 
-    /// The span location where the diagnostic occurred.
-    primary_highlight: Option<Highlight<L>>,
-
-    /// The message to display to the user.
-    message: String,
-
-    /// The optional help message to display to the user. This will be
-    /// displayed alongside the main message.
-    help_message: Option<String>,
-
-    /// List of related useful information to display to the user.
-    ///
-    /// For example, for unimplemented methods, this could be a list of
-    /// methods that need to be implemented.
-    related: Vec<Highlight<L>>,
+    /// The primary highlight of the diagnostic, which is the most important
+    /// region to be highlighted to the user.
+    group: Group<L>,
 
     /// List of related notes to display to the user.
     ///
@@ -136,13 +134,46 @@ impl<L> Rendered<L> {
     ) -> Self {
         Self {
             severity,
-            primary_highlight,
-            message,
-            help_message,
-            related,
+            group: Group { primary_highlight, message, help_message, related },
             notes,
         }
     }
+}
+
+impl<L> Rendered<L> {
+    /// Retrieves the severity of the diagnostic.
+    #[must_use]
+    pub const fn severity(&self) -> Severity { self.severity }
+
+    /// Retrieves the primary highlight of the diagnostic, if any.
+    #[must_use]
+    pub const fn primary_highlight(&self) -> Option<&Highlight<L>> {
+        self.group.primary_highlight.as_ref()
+    }
+
+    /// Retrieves the message of the diagnostic.
+    #[must_use]
+    pub fn message(&self) -> &str { &self.group.message }
+
+    /// Retrieves the help message of the diagnostic, if any.
+    #[must_use]
+    pub fn help_message(&self) -> Option<&str> {
+        self.group.help_message.as_deref()
+    }
+
+    /// Retrieves the related highlights of the diagnostic.
+    #[must_use]
+    pub fn related(&self) -> &[Highlight<L>] { &self.group.related }
+
+    /// Retrieves the related notes of the diagnostic.
+    #[must_use]
+    pub fn notes(&self) -> &[Note<L>] { &self.notes }
+
+    /// Retrieves the group of the diagnostic, which contains the primary
+    /// highlight, the message, the help message, and the related
+    /// highlights.
+    #[must_use]
+    pub const fn group(&self) -> &Group<L> { &self.group }
 }
 
 /// A struct used to attaching additional information to the main [`Rendered`]
@@ -158,26 +189,103 @@ impl<L> Rendered<L> {
     Encode,
     Decode,
     StableHash,
-    Builder,
     Identifiable,
 )]
 pub struct Note<L> {
+    group: Group<L>,
+}
+
+#[bon]
+impl<L> Note<L> {
+    /// Creates a builder for constructing a [`Rendered`] diagnostic.
+    #[builder(finish_fn = build)]
+    pub const fn builder(
+        primary_highlight: Option<Highlight<L>>,
+        #[builder(into)] message: String,
+        #[builder(into)] help_message: Option<String>,
+        #[builder(default = Vec::new())] related: Vec<Highlight<L>>,
+    ) -> Self {
+        Self {
+            group: Group { primary_highlight, message, help_message, related },
+        }
+    }
+}
+
+impl<L> Note<L> {
+    /// Retrieves the primary highlight of the note, if any.
+    #[must_use]
+    pub const fn primary_highlight(&self) -> Option<&Highlight<L>> {
+        self.group.primary_highlight.as_ref()
+    }
+
+    /// Retrieves the message of the note.
+    #[must_use]
+    pub fn message(&self) -> &str { &self.group.message }
+
+    /// Retrieves the help message of the note, if any.
+    #[must_use]
+    pub fn help_message(&self) -> Option<&str> {
+        self.group.help_message.as_deref()
+    }
+
+    /// Retrieves the related highlights of the note.
+    #[must_use]
+    pub fn related(&self) -> &[Highlight<L>] { &self.group.related }
+
+    /// Retrieves the group of the note, which contains the primary highlight,
+    /// the message, the help message, and the related highlights.
+    #[must_use]
+    pub const fn group(&self) -> &Group<L> { &self.group }
+}
+
+/// Represents a group of diagnostics that are related to each other and should
+/// be displayed together.
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    Encode,
+    Decode,
+    StableHash,
+)]
+pub struct Group<L> {
     /// The span location where the diagnostic occurred.
     primary_highlight: Option<Highlight<L>>,
 
     /// The message to display to the user.
-    #[builder(into)]
     message: String,
 
     /// The optional help message to display to the user. This will be
     /// displayed alongside the main message.
-    #[builder(into)]
     help_message: Option<String>,
 
     /// List of related useful information to display to the user.
     ///
     /// For example, for unimplemented methods, this could be a list of
     /// methods that need to be implemented.
-    #[builder(default = Vec::new())]
     related: Vec<Highlight<L>>,
+}
+
+impl<L> Group<L> {
+    /// Retrieves the primary highlight of the group, if any.
+    #[must_use]
+    pub const fn primary_highlight(&self) -> Option<&Highlight<L>> {
+        self.primary_highlight.as_ref()
+    }
+
+    /// Retrieves the message of the group.
+    #[must_use]
+    pub fn message(&self) -> &str { &self.message }
+
+    /// Retrieves the help message of the group, if any.
+    #[must_use]
+    pub fn help_message(&self) -> Option<&str> { self.help_message.as_deref() }
+
+    /// Retrieves the related highlights of the group.
+    #[must_use]
+    pub fn related(&self) -> &[Highlight<L>] { &self.related }
 }
