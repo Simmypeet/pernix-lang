f36f64c46afc69de7b7fd18d6055e77e01a71f05
Simmypeet <simmypeet@outlook.com>
2026-07-14 13:48:28 +0700

    refactor: Constraints to immutable ds

--- compiler/semantic/solver/src/constraints.rs
+++ compiler/semantic/solver/src/constraints.rs
@@ -1,6 +1,7 @@
 use std::hash::BuildHasherDefault;
 
-use pernixc_hash::FxHashSet;
+use im::HashSet;
+use pernixc_hash::FxHasher;
 use pernixc_qbice::Interner;
 use pernixc_type::{
     predicate::Outlives,
@@ -9,15 +10,18 @@ use pernixc_type::{
 };
 use qbice::storage::intern::Interned;
 
+type ConstraintSet = HashSet<Outlives, BuildHasherDefault<FxHasher>>;
+
+/// A collection of [`Outlives`] constraints.
+///
+/// Internally, it uses immutable data structures to allow for efficient sharing
+/// and O(1) cheap-to-clone operations.
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
-pub struct Constraints(FxHashSet<Outlives>);
+pub struct Constraints(ConstraintSet);
 
 impl Constraints {
     #[must_use]
-    pub fn union_into(mut self, other: Self) -> Self {
-        self.0.extend(other.0);
-        self
-    }
+    pub fn union_into(self, other: Self) -> Self { Self(self.0.union(other.0)) }
 }
 
 impl Substitutable for Constraints {
@@ -29,26 +33,20 @@ impl Substitutable for Constraints {
     where
         Self: Sized,
     {
-        let mut iter = self.0.iter().enumerate();
+        let iter = self.0.iter();
+        let mut new_constraints = None;
 
-        while let Some((index, next)) = iter.next() {
-            if let Some(changed) = next.apply(subst, interner) {
-                let mut new_constraints = FxHashSet::with_capacity_and_hasher(
-                    self.0.len(),
-                    BuildHasherDefault::default(),
-                );
+        for constraint in iter {
+            if let Some(new_constraint) = constraint.apply(subst, interner) {
+                let new_constraints =
+                    new_constraints.get_or_insert(self.0.clone());
 
-                new_constraints.extend(self.0.iter().take(index).cloned());
-                new_constraints.insert(changed);
-                new_constraints.extend(
-                    iter.map(|(_, c)| c.apply_or_clone(subst, interner)),
-                );
-
-                return Some(Self(new_constraints));
+                new_constraints.remove(constraint);
+                new_constraints.insert(new_constraint);
             }
         }
 
-        None
+        new_constraints.map(Self)
     }
 }
 
@@ -91,7 +89,7 @@ impl Extend<Outlives> for Constraints {
 
 impl IntoIterator for Constraints {
     type Item = Outlives;
-    type IntoIter = <FxHashSet<Outlives> as IntoIterator>::IntoIter;
+    type IntoIter = <ConstraintSet as IntoIterator>::IntoIter;
 
     fn into_iter(self) -> Self::IntoIter { self.0.into_iter() }
 }
